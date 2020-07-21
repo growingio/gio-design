e5c733c683f64b4edd4f2a09630eadd5719f70d1
Pan Jie <panjie@growingio.com>
2020-07-21 15:51:27 +0800

    refactor(tokens): update design tokens and change package structure
    affects: @gio-design/tokens, website

--- packages/tokens/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const { src, dest } = require('gulp');
-const yaml = require('gulp-yaml');
-
-const yaml2json = () =>
-  src('src/**/*.yml')
-  .pipe(yaml())
-  .pipe(dest('tmp'))
-
-exports.default = yaml2json
