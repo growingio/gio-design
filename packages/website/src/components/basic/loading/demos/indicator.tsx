91ca2c5370c84ac8b2423f940cf0feba48b735d2
lihang <lihang@growingio.com>
2020-07-29 15:20:52 +0800

    feat(loading): add loading component
    affects: @gio-design/components, @gio-design/tokens, website

---
+++ packages/website/src/components/basic/loading/demos/indicator.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Loading, Button } from '@gio-design/components';
+import { LoadingOutlinedWhite, LoadingOutlinedBlack } from '@gio-design/icons';
+import '@gio-design/components/es/components/loading/style/index.css';
+import '@gio-design/components/es/components/button/style/index.css';
+import './index.less';
+
+export default () => (
+  <div className="displayBaseLoading">
+    <Button
+      icon={
+        <Loading
+          titlePosition="right"
+          indicator={<LoadingOutlinedWhite />}
+          title={false}
+        />
+      }
+      style={{ lineHeight: 0 }}
+    />
+
+    <Button
+      disabled
+      icon={
+        <Loading
+          titlePosition="right"
+          indicator={<LoadingOutlinedBlack />}
+          title={false}
+        />
+      }
+      style={{ lineHeight: 0 }}
+    />
+  </div>
+);
---
+++ packages/website/src/components/basic/loading/demos/wrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Loading } from '@gio-design/components';
+import Tabs from '../../tabs/demo/block.tsx';
+import '@gio-design/components/es/components/loading/style/index.css';
+import './index.less';
+
+export default () => (
+  <Loading titlePosition="right">
+    <Tabs />
+  </Loading>
+);
