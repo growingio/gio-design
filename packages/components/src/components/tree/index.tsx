edb6cdad741e6b8c7595d3d7bcbb2153fc31b912
huskylengcb <649569822@qq.com>
2020-08-04 17:38:45 +0800

    feat(tree): add tree component (#51)
    affects: @gio-design/components, @gio-design/icons, @gio-design/tokens, website

---
+++ packages/components/src/components/tree/index.tsx
@@ -0,0 +1,15 @@
+import Tree from './Tree';
+
+export { EventDataNode } from 'rc-tree/lib/interface';
+
+export {
+  TreeProps,
+  TreeNodeNormal,
+  GioTreeNode,
+  GioTreeNodeMouseEvent,
+  GioTreeNodeExpandedEvent,
+  GioTreeNodeSelectedEvent,
+  GioTreeNodeProps,
+} from './Tree';
+
+export default Tree;
--- packages/icons/src/More.tsx
+++ packages/icons/src/More.tsx
@@ -3,12 +3,11 @@ import * as React from 'react';
 function SvgMore(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox='0 0 14 14' width='1em' height='1em' {...props}>
-      <g id='more'>
-        <path
-          d='M7,1A1,1,0,1,1,6,2,1,1,0,0,1,7,1ZM7,6A1,1,0,1,1,6,7,1,1,0,0,1,7,6Zm0,5a1,1,0,1,1-1,1A1,1,0,0,1,7,11Z'
-          style={{ fill: 'currentColor' }}
-        />
-      </g>
+      <path
+        className='more_svg__cls-2'
+        d='M7 1a1 1 0 11-1 1 1 1 0 011-1zm0 5a1 1 0 11-1 1 1 1 0 011-1zm0 5a1 1 0 11-1 1 1 1 0 011-1z'
+        fill='currentColor'
+      />
     </svg>
   );
 }
