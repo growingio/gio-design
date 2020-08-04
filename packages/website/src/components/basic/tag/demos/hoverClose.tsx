92584ec0204cceea4207668e54ea3cf000b06d52
Zhou <zhouzhaoting@growingio.com>
2020-08-04 17:29:58 +0800

    fix(website): fix hover close example;
    affects: website

--- packages/website/src/components/basic/tag/demos/hoverClose.tsx
+++ packages/website/src/components/basic/tag/demos/hoverClose.tsx
@@ -7,7 +7,7 @@ const onClose = (e: any) => console.log('you have clicked tag');
 
 const HoverClose = () => (
   <>
-    <Tag className="tag" closable={true} onClose={onClose}>
+    <Tag className="tag" closable={true} persistCloseIcon={false} onClose={onClose}>
       可删除的标签
     </Tag>
     <Tag className="tag" closable={true} disabled>
