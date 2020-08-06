5b37e5fdb5089365be3334148f14e858e03264b1
liudan <liudan@growingio.com>
2020-08-06 19:15:27 +0800

    添加alert-size

--- packages/website/src/components/basic/alert/demo/alertSize.tsx
+++ packages/website/src/components/basic/alert/demo/alertSize.tsx
@@ -4,9 +4,9 @@ import '@gio-design/components/es/components/alert/style/index.less';
 
 export default () => (
   <div>
-    <Alert message="Info Text" />
-    <Alert type="success" message="Success Text" />
-    <Alert type="warning" message="Warning Text" />
-    <Alert type="error" message="Error Text" />
+    <Alert message="Info Text" size="small" />
+    <Alert type="success" message="Success Text" size="small" />
+    <Alert type="warning" message="Warning Text" size="small" />
+    <Alert type="error" message="Error Text" size="small" />
   </div>
 );
