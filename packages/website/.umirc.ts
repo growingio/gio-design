8794245b00b568c4be433d5a0349d267668fdc42
Jack <i@jackpan.me>
2020-08-10 20:47:47 +0800

    docs(website): components & tokens 20.8.1 and icons 20.8.0 changelog (#65)
    affects: @gio-design/components, @gio-design/icons, playground, @gio-design/themes,
    @gio-design/tokens, website

--- packages/website/.umirc.ts
+++ packages/website/.umirc.ts
@@ -3,7 +3,11 @@ export default {
   title: 'GrowingIO Design',
   logo: 'https://avatars1.githubusercontent.com/u/10370210?s=400&v=4',
   favicon: 'https://www.growingio.com/vassets/images/ab6cf9a60362c74acf9c252fa35ea7cc-favicon.png',
-  locales: [['zh-CN', '简体中文'], ['en-US', 'English']],
+  locales: [
+    ['zh-CN', '简体中文'],
+    ['en-US', 'English'],
+  ],
   hash: true,
   exportStatic: {},
+  ssr: {},
 };
