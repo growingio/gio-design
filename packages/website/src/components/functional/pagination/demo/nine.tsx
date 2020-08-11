e7d8efa5a017499a2fab90c81b80705a9c0fab8c
lihang <lihang@growingio.com>
2020-08-11 15:16:56 +0800

    feat(pagaination): add pagaination component
    affects: @gio-design/components, @gio-design/icons, @gio-design/tokens, website

---
+++ packages/website/src/components/functional/pagination/demo/nine.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Pagination } from '@gio-design/components';
+import '@gio-design/components/es/components/pagination/style/index.css';
+
+export default () => <Pagination total={90} />;
