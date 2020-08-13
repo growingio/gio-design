4f475053ddd7c25dede301c00637337028ab7258
zhaoting zhou <zhouzhaoting@growingio.com>
2020-08-13 19:19:59 +0800

    refactor(tag): refactor tag with new tokens (#93)

--- packages/website/src/components/basic/tag/demos/basic.tsx
+++ packages/website/src/components/basic/tag/demos/basic.tsx
@@ -5,17 +5,19 @@ import './index.less';
 
 const Basics = () => (
   <>
-    <Tag className="tag_website_demo_tag">超管</Tag>
-    <Tag className="tag_website_demo_tag" status="success">
+    <Tag className="tag_website_demo_tag" size="small">
+      超管
+    </Tag>
+    <Tag className="tag_website_demo_tag" status="success" size="small">
       已上线
     </Tag>
-    <Tag className="tag_website_demo_tag" status="warning">
+    <Tag className="tag_website_demo_tag" status="warning" size="small">
       待上线
     </Tag>
-    <Tag className="tag_website_demo_tag" status="draft">
+    <Tag className="tag_website_demo_tag" status="draft" size="small">
       草稿
     </Tag>
-    <Tag className="tag_website_demo_tag" status="offline">
+    <Tag className="tag_website_demo_tag" status="offline" size="small">
       已结束
     </Tag>
   </>
--- packages/website/src/components/basic/tag/demos/closable.tsx
+++ packages/website/src/components/basic/tag/demos/closable.tsx
@@ -3,17 +3,15 @@ import { Tag } from '@gio-design/components';
 import '@gio-design/components/es/components/tag/style/index.css';
 import './index.less';
 
-const onClose = (e: any) => console.log('you have clicked tag');
-
 const Closable = () => (
   <>
-    <Tag className="tag_website_demo_tag" closable={true} persistCloseIcon={true} onClose={onClose}>
+    <Tag className="tag_website_demo_tag" closable={true}>
       控件内的过滤条件
     </Tag>
-    <Tag className="tag_website_demo_tag" color="blue" closable={true} persistCloseIcon={true} onClose={onClose}>
+    <Tag className="tag_website_demo_tag" type="prorupt" closable={true} persistCloseIcon={true}>
       控件内的过滤条件
     </Tag>
-    <Tag className="tag_website_demo_tag" type="large" closable={true} persistCloseIcon={true} onClose={onClose}>
+    <Tag className="tag_website_demo_tag" size="large" closable={true}>
       控件内的过滤条件
     </Tag>
   </>
