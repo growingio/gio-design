05141aa869bbb7a0df4297c78d6362adae866c3a
itiiss <kurarin@outlook.com>
2020-08-21 16:50:10 +0800

    style(banner): fix style

--- packages/website/src/components/functional/banner/demo/alert.tsx
+++ packages/website/src/components/functional/banner/demo/alert.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Banner from '@gio-design/components/es/components/banner';
 import { Link } from '@gio-design/components';
 import '@gio-design/components/es/components/banner/style/index.css';
+import './index.less';
 
 const alertContent = (
-  <div>
+  <div className="alert-close">
     尊敬的客户您好，工单系统将于2月2日～2月10日暂停服务。
     <Link to="#">查看详情</Link>
   </div>
--- packages/website/src/components/functional/banner/demo/closeable.tsx
+++ packages/website/src/components/functional/banner/demo/closeable.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Banner from '@gio-design/components/es/components/banner';
-import Button from '@gio-design/components/es/components/button';
+import { Tag, Button } from '@gio-design/components';
 import '@gio-design/components/es/components/banner/style/index.css';
 
-const normalContent = '试用14天后结束';
+const normalContent = (
+  <div>
+    <Tag color="beta" size="small" style={{ margin: '0 8px' }}>
+      试用中
+    </Tag>
+    试用14天后结束
+  </div>
+);
 const button = (
   <Button type="secondary" size="small">
     商务咨询
