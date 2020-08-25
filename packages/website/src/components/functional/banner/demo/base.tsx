8aa1bf31d15cdd4d9443dd0ee74f6537fd9bb85c
itiiss <kurarin@outlook.com>
2020-08-25 11:58:34 +0800

    style(banner): demo style

--- packages/website/src/components/functional/banner/demo/base.tsx
+++ packages/website/src/components/functional/banner/demo/base.tsx
@@ -3,11 +3,13 @@ import Banner from '@gio-design/components/es/components/banner';
 import Button from '@gio-design/components/es/components/button';
 import '@gio-design/components/es/components/banner/style/index.css';
 
-const normalContent = '【GrowingIO在线公共课】欧治云商运营负责人复盘B2B增长实践';
-const button = (
-  <Button type="secondary" size="small">
-    立即报名
-  </Button>
+const normalContent = (
+  <div className="alert-close">
+    【GrowingIO在线公共课】欧治云商运营负责人复盘B2B增长实践
+    <Button type="secondary" size="small" style={{ margin: '0 0 0 8px' }}>
+      立即报名
+    </Button>
+  </div>
 );
 
-export default () => <Banner content={normalContent} type="normal" closeable={true} button={button} />;
+export default () => <Banner content={normalContent} type="normal" closeable={true} />;
