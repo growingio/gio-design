da5a8784ba5cc60babd97de63f261c58411efbfc
phyzess <phyzess@gmail.com>
2020-09-08 11:51:34 +0800

    refactor(components): fix test case
    affects: @gio-design/components

--- packages/components/src/components/upload/__tests__/upload.test.js
+++ packages/components/src/components/upload/__tests__/upload.test.js
@@ -1,52 +1,43 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { setup, clear } from '../tests/mock';
 import { mountTest, mountSnapshot } from '../tests/mount';
 import Upload from '..';
 
 const uploadTypes = ['button', 'input', 'card', 'avatar', 'drag'];
 
-const mockImage = {
-  target: {
-    files: [{ file: 'foo.png' }],
-  },
-};
-
-describe('Testing Upload', () => {
+describe('Testing Upload mount', () => {
   uploadTypes.forEach((type) => {
     mountTest(type);
     mountSnapshot(type);
   });
+});
 
+describe('Testing Upload actions', () => {
   beforeEach(() => setup());
   afterEach(() => clear());
 
-  it('return promise in beforeUpload', async (done) => {
-    const props = {
-      action: '/upload',
-      beforeUpload: () =>
-        new Promise((resolve) =>
-          setTimeout(() => {
-            resolve('success');
-          }, 100)
-        ),
-      onSuccess: (res, file) => {
-        if (file.status !== 'uploading') {
-          done();
-        }
+  it('return promise in beforeUpload', (done) => {
+    const wrapper = mount(
+      <Upload
+        action="http://upload.com"
+        beforeUpload={() => new Promise((resolve) => setTimeout(() => resolve('success'), 100))}
+        onSuccess={(res, file) => {
+          if (file.status !== 'uploading') {
+            done();
+          }
+        }}
+        onError={(error, file) => {
+          if (file.status !== 'uploading') {
+            done();
+          }
+        }}
+      />
+    );
+    wrapper.find('input').simulate('change', {
+      target: {
+        files: [{ file: 'foo.png' }],
       },
-      onError: (error, file) => {
-        if (file.status !== 'uploading') {
-          done();
-        }
-      },
-    };
-
-    let wrapper;
-    await act(async () => {
-      wrapper = mount(<Upload {...props} />);
-      wrapper.find('input').simulate('change', mockImage);
     });
   });
 });
