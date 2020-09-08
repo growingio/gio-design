89e9482f385ee2e66b0ede83a74da8f75b8c3000
Pan Jie <panjie@growingio.com>
2020-09-08 16:19:24 +0800

    style: fix input and search bar stylelint errors
    affects: @gio-design/components

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   // Rewrite style
   rules: {
     'no-use-before-define': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'import/no-unresolved': 0,
     'import/extensions': [0, 'never'],
   },
