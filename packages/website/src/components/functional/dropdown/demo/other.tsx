b09ac44c2e980d70c622e175596ed7c92438d307
lihang <lihang@growingio.com>
2020-09-10 10:58:36 +0800

    style(checkbox): align check icon when be used to Table
    affects: @gio-design/components, website

--- packages/website/src/components/functional/dropdown/demo/other.tsx
+++ packages/website/src/components/functional/dropdown/demo/other.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Dropdown, Button } from '@gio-design/components';
 import { More } from '@gio-design/icons';
-import '@gio-design/components/es/components/Dropdown/style/index.css';
+import '@gio-design/components/es/components/dropdown/style/index.css';
 
 export default () => (
   <Dropdown overlay={<div style={{ width: 200 }}>11111</div>}>
