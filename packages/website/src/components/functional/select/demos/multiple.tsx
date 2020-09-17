237f92563e08c377f21fef5ae6ebd3c43752ee6f
Zhou <zhouzhaoting@growingio.com>
2020-09-17 17:27:50 +0800

    fix(onchange bug): onchange bug
    affects: @gio-design/components, website

--- packages/website/src/components/functional/select/demos/multiple.tsx
+++ packages/website/src/components/functional/select/demos/multiple.tsx
@@ -4,6 +4,6 @@ import '@gio-design/components/es/components/select/style/index.css';
 
 import options from './options';
 
-const Basics = (): React.ReactNode => <Select options={options} multiple width={200} />;
+const Basics = (): React.ReactNode => <Select options={options} multiple width={200} onChange={console.log} />;
 
 export default Basics;
