208c3aac59ebe70ce726abe32371d95961f56e31
WORLDI <hejiangas@163.com>
2020-10-23 15:19:25 +0800

    test(alert): improve test coverage to 100%
    affects: @gio-design/components
    
    improve test coverage to 100%

--- packages/components/src/components/alert/__tests__/alert.test.js
+++ packages/components/src/components/alert/__tests__/alert.test.js
@@ -1,60 +1,27 @@
 import React from 'react';
 import { mount, render } from 'enzyme';
+import { TagOutlined } from '@gio-design/icons'
 import Alert from '..'
 
 describe('Alert snapshots.',() => {
     it('should match alert base snapshot.',() => {
         const wrapper = render(
-          <Alert type="success" message="Success Text" />
+          <Alert 
+            type="success" 
+            message="Success Text" 
+          />
         )
         expect(wrapper).toMatchSnapshot();
     })
 
-    it('should match various styles alert snapshot.',() => {
-        const wrapper = render(
-          <div>
-            <Alert message="Info Text" description="Info Description Info Description Info Description Info Description" />
-            <Alert
-              type="success"
-              message="Success Text"
-              description="Success Description Success Description Success Description"
-            />
-            <Alert
-              type="warning"
-              message="Warning Text"
-              description="Warning Description Warning Description Warning Description Warning Description"
-            />
-            <Alert
-              type="error"
-              message="Error Text"
-              description="Error Description Error Description Error Description Error Description"
-            />
-            <Alert
-              message="Info Text"
-              description="Info Description Info Description Info Description Info Description"
-              size="small"
-            />
-            <Alert
-              type="success"
-              message="Success Text"
-              description="Success Description Success Description Success Description"
-              size="small"
-            />
-            <Alert
-              type="warning"
-              message="Warning Text"
-              description="Warning Description Warning Description Warning Description Warning Description"
-              size="small"
-            />
-            <Alert
-              type="error"
-              message="Error Text"
-              description="Error Description Error Description Error Description Error Description"
-              size="small"
-            />
-          </div>
-        );
-        expect(wrapper).toMatchSnapshot();
+    it('icon by self.',() => {
+      const wrapper = render(
+        <Alert 
+          icon={<TagOutlined />}
+          showIcon
+        />
+      )
+      expect(wrapper).toMatchSnapshot();
     })
 
     it('close function should be called when click the closeIcon.',() => {
@@ -71,37 +38,23 @@ describe('Alert snapshots.',() => {
         expect(onClickMock).toBeCalled();
     })
 
-    it('alert should be closed when click the closeIcon.',() => {
-        const onClickMock = jest.fn();
-        const wrapper = mount(
-          <Alert
-            message="Info Text"
-            description="Info Description Info Description Info Description Info Description"
-            closeable
-            onClose={onClickMock}
-          />
-        )
-        wrapper.find('.gio-alert-closeIcon').simulate('click');
-        expect(wrapper.find('.gio-alert').hasClass('gio-alert-close')).toBeTruthy();
-    })
-
     it('should show icons',() => {
         const wrapper = render(
           <div>
-            <Alert message="Info Text" showIcon />
+            <Alert type="info" message="Info Text" showIcon />
             <Alert type="success" message="Success Text" showIcon />
             <Alert type="warning" message="Warning Text" showIcon />
             <Alert type="error" message="Error Text" showIcon />
-            <Alert message="Info Text" showIcon size="small" />
+            <Alert type="info" message="Info Text" showIcon size="small" />
             <Alert type="success" message="Success Text" showIcon size="small" />
             <Alert type="warning" message="Warning Text" showIcon size="small" />
             <Alert type="error" message="Error Text" showIcon size="small" />
           </div>
         )
-        expect(wrapper.find('.gio-alert-icon')).toBeDefined();
+        expect(wrapper).toMatchSnapshot();
     })
 
-    it('the colseText replace closeIcon should be useful',() => {
+    it('the colseText should be useful',() => {
         const wrapper = mount(
           <Alert
             type="success"
@@ -113,4 +66,32 @@ describe('Alert snapshots.',() => {
         wrapper.find('.gio-alert-closeIcon').simulate('click');
         expect(wrapper.find('.gio-alert').hasClass('gio-alert-close')).toBeTruthy();
     })
+
+    it('should be stable',() => {
+      const wrapper = render(
+        <div>
+          <Alert description="Info Description" />
+          <Alert type="success" description="Success Description" />
+          <Alert
+            type="warning"
+            description="Warning Description"
+          />
+          <Alert type="error" description="Error Description" />
+          <Alert description="Info Description" size="small" />
+          <Alert type="success" description="Success Description" size="small" />
+          <Alert
+            type="warning"
+            description="Warning Description"
+            size="small"
+          />
+          <Alert
+            type="error"
+            description="Error Description"
+            size="small"
+          />
+        </div>
+      )
+      expect(wrapper).toMatchSnapshot();
+    })
+
 })
