36c489e35a43bf902a5ea92b71c910b4d00fa8a2
i18u <iahu1988@gmail.com>
2020-10-26 12:56:07 +0800

    feat: add some icons (#349)
    * feat: add some icons
    
    * chore: revert close.svg
    
    * chore: remove CloseOutlined

---
+++ packages/icons/src/InformationCircleOutlined.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Wrapper from './Wrapper';
+import { IconProps } from './interface';
+
+function SvgInformationCircleOutlined(wrapperProps: IconProps) {
+  const { rotating, color, size, ...restProps } = wrapperProps;
+  const props = {
+    color,
+    className: rotating ? 'gio-icon-svg gio-icon-rotating' : 'gio-icon-svg',
+    width: !size ? '1rem' : size,
+    height: !size ? '1rem' : size,
+  };
+  const file = (
+    <svg viewBox="64 64 896 896" aria-hidden="true" fill="currentColor" {...props}>
+      <path
+        d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"
+        fill="currentColor"
+      />
+      <path
+        d="M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+  return <Wrapper {...restProps} icon={file} />;
+}
+
+export default SvgInformationCircleOutlined;
