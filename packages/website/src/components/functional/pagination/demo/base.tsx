9e088fa1f21f4ed78ffb39dd8d8840291000135d
lihang <lihang@growingio.com>
2020-10-28 10:52:26 +0800

    test(pagination): improve test coverage to 99%
    affects: @gio-design/components

--- packages/website/src/components/functional/pagination/demo/base.tsx
+++ packages/website/src/components/functional/pagination/demo/base.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { Pagination } from '@gio-design/components';
 import '@gio-design/components/es/components/pagination/style/index.css';
 
-export default () => <Pagination total={1100} />;
+export default () => <Pagination total={110} />;
