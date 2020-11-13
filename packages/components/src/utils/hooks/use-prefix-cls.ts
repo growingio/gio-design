fa6bbc01146f9b6891f7ab73e0a69cb0306307f2
i18u <iahu1988@gmail.com>
2020-11-13 12:59:05 +0800

    fix: import

--- packages/components/src/utils/hooks/use-prefix-cls.ts
+++ packages/components/src/utils/hooks/use-prefix-cls.ts
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+
+import { ConfigContext } from '../../components/config-provider';
+
 const usePrefixCls = (suffixCls?: string, customizePrefixCls?: string): string => {
   const { rootPrefixCls } = useContext(ConfigContext);
   const { customizePrefixCls: prefixCls = rootPrefixCls } = { customizePrefixCls };
