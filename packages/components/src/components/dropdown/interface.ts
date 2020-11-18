f208f1480f765aadeda18df2f3c12d041887300f
WORLDI <hejiangas@163.com>
2020-11-18 15:45:44 +0800

    docs: add stories to some Functional components
    affects: @gio-design/components
    
    add stories to some Functional components

--- packages/components/src/components/dropdown/interface.ts
+++ packages/components/src/components/dropdown/interface.ts
@@ -1,5 +1,8 @@
 import { TooltipProps } from '../tooltip/interface';
 
 export interface DropdownProps extends Omit<TooltipProps, 'title' | 'tooltipLink' | 'arrowPointAtCenter' | 'overlay'> {
+  /**
+   下拉区域
+   */
   overlay: (() => React.ReactElement) | React.ReactElement;
 }
