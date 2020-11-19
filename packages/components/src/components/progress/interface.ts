b92659a22f0c8f75bef347591685a35eb0daf268
Qiyao Hu <qiyao@QiyaodeMacBook-Pro.local>
2020-11-19 11:32:41 +0800

    docs(components): add annotations
    affects: @gio-design/components

--- packages/components/src/components/progress/interface.ts
+++ packages/components/src/components/progress/interface.ts
@@ -15,7 +15,6 @@ export interface ProgressProps {
   status?: keyof typeof ProgressStatus | string;
   /**
    * 内容模版函数
-   * @default (percent?: number) => React.ReactNode;
    */
   format?: (percent?: number) => React.ReactNode;
   customizePrefixCls?: string;
