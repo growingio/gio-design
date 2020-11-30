2a5067ce729f4aa193b55e46347abb91cffd3121
phyzess <phyzess@gmail.com>
2020-11-30 19:45:52 +0800

    test(modal): fix test case
    affects: @gio-design/components

--- packages/components/src/components/modal/__test__/staticFunc.test.js
+++ packages/components/src/components/modal/__test__/staticFunc.test.js
@@ -179,6 +179,7 @@ describe('Modal.staticFunc triggers correctly.', () => {
   });
 
   it('could be destroy', async () => {
+    jest.useFakeTimers();
     ['info', 'success', 'warn', 'error'].forEach(async (type) => {
       const instance = Modal[type]({
         title: 'title',
