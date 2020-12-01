4a5de2af6ef7eedaff2763700709443a7bb01c46
phyzess <phyzess@gmail.com>
2020-12-01 11:11:55 +0800

    feat(menu): add verticalIndent prop
    affects: @gio-design/components, website

--- packages/components/src/components/menu/SubMenu.tsx
+++ packages/components/src/components/menu/SubMenu.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SubMenu as RcSubMenu } from 'rc-menu';
 import { ISubMenuProps } from './interface';
 import MenuTitle from './MenuTitle';
+import { MenuContext, SubMenuContext } from './MenuContext';
 
 const SubMenu: React.FC<ISubMenuProps> = (props: ISubMenuProps) => {
   const { icon, title, ...restProps } = props;
+  const { verticalIndent } = useContext(MenuContext);
 
-  const renderTitle = () => <MenuTitle title={title} icon={icon} />;
+  const renderTitle = (): React.ReactElement => <MenuTitle title={title} icon={icon} />;
 
-  return <RcSubMenu {...restProps} title={renderTitle()} inlineIndent={16} disabled />;
+  return (
+    <SubMenuContext.Provider value={{ inSubMenu: true, inIconSubMenu: !!icon }}>
+      <RcSubMenu {...restProps} title={renderTitle()} inlineIndent={verticalIndent} disabled />
+    </SubMenuContext.Provider>
+  );
 };
 
 SubMenu.displayName = 'GIODesignSubMenu';
