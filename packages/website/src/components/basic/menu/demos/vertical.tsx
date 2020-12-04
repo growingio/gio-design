f3dbab9cb12ccd4468918fc3cd355c72aa45c968
WORLDI <66404428+WORLDI@users.noreply.github.com>
2020-12-04 22:06:28 +0800

    fix(table): add border-radius to table when placeholder (#569)
    affects: @gio-design/components, website
    
    Co-authored-by: Jack <panjie@growingio.com>

--- packages/website/src/components/basic/menu/demos/vertical.tsx
+++ packages/website/src/components/basic/menu/demos/vertical.tsx
@@ -29,8 +29,27 @@ export default () => {
             功能名称-2-2
           </MenuItem>
         </SubMenu>
-        <MenuItem key="sub-menu-3">功能名称-3</MenuItem>
-        <MenuItem key="sub-menu-4" icon={<MapChartOutlined />}>
+        <SubMenu key="sub-3" title="功能名称-3" icon={<AppOutlined />}>
+          <MenuItem key="sub-menu-3-1">功能名称-3-1</MenuItem>
+          <MenuItem key="sub-menu-3-2" icon={<MapChartOutlined />}>
+            功能名称-3-2
+          </MenuItem>
+        </SubMenu>
+        <SubMenu key="sub-4" title="功能名称-4" icon={<AppOutlined />}>
+          <MenuItem key="sub-menu-4-1">功能名称-4-1</MenuItem>
+          <MenuItem key="sub-menu-4-2" icon={<MapChartOutlined />}>
+            功能名称-4-2
+          </MenuItem>
+        </SubMenu>
+      </Menu>
+      <hr />
+      <Menu className="demo-menu-vertical" mode="vertical" selectedKey={selectedKey} onClick={handleClick}>
+        <MenuItem key="menu-item-1">功能名称-1</MenuItem>
+        <MenuItem key="menu-item-2">功能名称-2</MenuItem>
+        <MenuItem key="menu-item-3" icon={<MapChartOutlined />}>
+          功能名称-3
+        </MenuItem>
+        <MenuItem key="menu-item-4" icon={<MapChartOutlined />}>
           功能名称-4
         </MenuItem>
       </Menu>
