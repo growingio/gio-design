growingio/gio-design

commit f49776935edfdf2d663353008f9c26fbea8b6c70

style(avatar): fix scale error, capitalise the first etter (#617)
affects: @gio-design/components

Co-authored-by: Jack <panjie@growingio.com>

packages/components/src/components/avatar/Avatar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import classNames from 'classnames';
 import { More } from '@gio-design/icons';
-import { isNil } from 'lodash';
+import { isNil, isUndefined } from 'lodash';
 import Tooltip from '../tooltip';
 import { AvatarProps } from './interfaces';
 import usePrefixCls from '../../utils/hooks/use-prefix-cls';
@@ -29,12 +29,16 @@ const Avatar = React.forwardRef<HTMLSpanElement, AvatarProps>((props: AvatarProp
   const mergedRef = composeRef(ref, nodeRef);
 
   useEffect(() => {
+    setIsImgExist(!isUndefined(src));
+  }, [src]);
+
+  useEffect(() => {
     if (nodeRef.current && childrenRef.current) {
       const nodeWidth = nodeRef.current.offsetWidth;
       const childrenWidth = childrenRef.current.offsetWidth;
       setScale(nodeWidth - 8 < childrenWidth ? (nodeWidth - 8) / childrenWidth : 1);
     }
-  }, [userName]);
+  }, [userName, isImgExist]);
 
   const prefixCls = usePrefixCls('avatar', customizePrefixCls);
   const classString = classNames(className, prefixCls, {
@@ -45,7 +49,7 @@ const Avatar = React.forwardRef<HTMLSpanElement, AvatarProps>((props: AvatarProp
   });
 
   const childrenStyle: React.CSSProperties = {
-    transform: `scale(${scale})`,
+    transform: `scale(${scale}) translateX(-50%)`,
   };
 
   const renderMore = () => {
@@ -64,7 +68,7 @@ const Avatar = React.forwardRef<HTMLSpanElement, AvatarProps>((props: AvatarProp
       return <img alt="avatar" src={src} onError={() => setIsImgExist(false)} />;
     }
     if (userName !== undefined && typeof userName === 'string') {
-      const prefixUserName = omit && typeof userName === 'string' ? userName.trim()[0] : userName.trim();
+      const prefixUserName = omit && typeof userName === 'string' ? userName.trim()[0].toUpperCase() : userName.trim();
       return (
         <span ref={childrenRef} style={childrenStyle}>
           {prefixUserName}

packages/components/src/components/avatar/style/index.less
@@ -43,10 +43,9 @@
   }
 
   span {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
+    position: absolute;
+    left: 50%;
+    transform-origin: 0 center;
   }
 
   img {
@@ -95,7 +94,9 @@
   &-group {
     .@{avatar-prefix-cls} {
       border: 1.5px solid @color-border-avatar;
-
+      span {
+        top: -1.5px;
+      }
       &:hover {
         position: relative;
         z-index: 1;

packages/components/src/components/avatar/__tests__/__snapshots__/Avatar.test.js.snap
@@ -7,7 +7,7 @@ exports[`Testing Avatar should be stable 1`] = `
   <span
     style={
       Object {
-        "transform": "scale(1)",
+        "transform": "scale(1) translateX(-50%)",
       }
     }
   >

packages/components/src/components/avatar/__tests__/__snapshots__/AvatarGroup.test.js.snap
@@ -25,11 +25,11 @@ exports[`Testing AvatarGroup should be stable 1`] = `
     <span
       style={
         Object {
-          "transform": "scale(1)",
+          "transform": "scale(1) translateX(-50%)",
         }
       }
     >
-      p
+      P
     </span>
   </span>
   <span
@@ -53,11 +53,11 @@ exports[`Testing AvatarGroup should be stable 1`] = `
     <span
       style={
         Object {
-          "transform": "scale(1)",
+          "transform": "scale(1) translateX(-50%)",
         }
       }
     >
-      l
+      L
     </span>
   </span>
   <span
@@ -66,7 +66,7 @@ exports[`Testing AvatarGroup should be stable 1`] = `
     <span
       style={
         Object {
-          "transform": "scale(1)",
+          "transform": "scale(1) translateX(-50%)",
         }
       }
     >

packages/components/src/components/avatar/__tests__/Avatar.test.js
@@ -32,10 +32,10 @@ describe('Testing Avatar', () => {
   test('props src', () => {
     const wrapper = mount(<Avatar src={image}>li</Avatar>).children();
     expect(wrapper.childAt(0).type()).toBe('img');
-    expect(wrapper.childAt(0).text()).not.toBe('l');
+    expect(wrapper.childAt(0).text()).not.toBe('L');
     const noSrcWrapper = mount(<Avatar size="small">li</Avatar>).children();
     expect(noSrcWrapper.childAt(0).type()).toBe('span');
-    expect(noSrcWrapper.childAt(0).text()).toBe('l');
+    expect(noSrcWrapper.childAt(0).text()).toBe('L');
   });
 
   test('props size', () => {

packages/components/src/components/avatar/__tests__/AvatarGroup.test.js
@@ -43,10 +43,10 @@ describe('Testing AvatarGroup', () => {
     wrapper = wrapper.find('.gio-avatar');
     expect(wrapper.at(0).childAt(0).type()).toBe('img');
     expect(wrapper.at(1).childAt(0).type()).toBe('span');
-    expect(wrapper.at(1).childAt(0).text()).toBe('p');
+    expect(wrapper.at(1).childAt(0).text()).toBe('P');
     expect(wrapper.at(2).childAt(0).type()).toBe('img');
     expect(wrapper.at(3).childAt(0).type()).toBe('span');
-    expect(wrapper.at(3).childAt(0).text()).toBe('l');
+    expect(wrapper.at(3).childAt(0).text()).toBe('L');
     expect(wrapper.at(4).childAt(0).text()).toBe(`+${users.length - 4}`);
   });
 

packages/components/src/components/avatar/style/index.ts
@@ -1,1 +1,2 @@
 import './index.less';
+import '../../tooltip/index';

commit 65daec7de3aaad576566291dc4073b3401948f5c

fix(avatar): fix style error caused by #617
affects: @gio-design/components

packages/components/src/components/avatar/Avatar.tsx
@@ -70,7 +70,7 @@ const Avatar = React.forwardRef<HTMLSpanElement, AvatarProps>((props: AvatarProp
     if (userName !== undefined && typeof userName === 'string') {
       const prefixUserName = omit && typeof userName === 'string' ? userName.trim()[0].toUpperCase() : userName.trim();
       return (
-        <span ref={childrenRef} style={childrenStyle}>
+        <span ref={childrenRef} style={childrenStyle} className={`${prefixCls}-string`}>
           {prefixUserName}
         </span>
       );

packages/components/src/components/avatar/style/index.less
@@ -13,13 +13,10 @@
   opacity: 1;
   user-select: none;
 
-  &:hover {
-    .@{avatar-prefix-cls}-droppable {
-      background-color: @color-background-avatar-hover-dropdown;
-
-      svg {
-        transform: scale(1);
-      }
+  &:hover &-droppable {
+    background-color: @color-background-avatar-hover-dropdown;
+    span {
+      transform: scale(1);
     }
   }
 
@@ -35,14 +32,14 @@
     cursor: pointer;
     transition: all 0.3s;
 
-    svg {
+    span {
       vertical-align: middle;
       transform: scale(0);
       transition: all 0.3s;
     }
   }
 
-  span {
+  &-string {
     position: absolute;
     left: 50%;
     transform-origin: 0 center;

packages/components/src/components/avatar/__tests__/__snapshots__/Avatar.test.js.snap
@@ -5,6 +5,7 @@ exports[`Testing Avatar should be stable 1`] = `
   className="gio-avatar gio-avatar-df"
 >
   <span
+    className="gio-avatar-string"
     style={
       Object {
         "transform": "scale(1) translateX(-50%)",

packages/components/src/components/avatar/__tests__/__snapshots__/AvatarGroup.test.js.snap
@@ -23,6 +23,7 @@ exports[`Testing AvatarGroup should be stable 1`] = `
     onMouseLeave={[Function]}
   >
     <span
+      className="gio-avatar-string"
       style={
         Object {
           "transform": "scale(1) translateX(-50%)",
@@ -51,6 +52,7 @@ exports[`Testing AvatarGroup should be stable 1`] = `
     onMouseLeave={[Function]}
   >
     <span
+      className="gio-avatar-string"
       style={
         Object {
           "transform": "scale(1) translateX(-50%)",
@@ -64,6 +66,7 @@ exports[`Testing AvatarGroup should be stable 1`] = `
     className="gio-avatar-rest gio-avatar gio-avatar-df"
   >
     <span
+      className="gio-avatar-string"
       style={
         Object {
           "transform": "scale(1) translateX(-50%)",