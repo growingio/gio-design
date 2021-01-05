e5feb76ee3649cc7c94d4f9f950a6b3ce58b9348
Dan <liudan548326@yeah.net>
2021-01-05 17:27:46 +0800

    fix(toggles): 修复toggles组件checked不可控 (#689)
    Co-authored-by: liudan <liudan@growingio.com>
    Co-authored-by: Jack <panjie@growingio.com>

--- website/src/components/basic/toggles/demo/toggles.tsx
+++ website/src/components/basic/toggles/demo/toggles.tsx
@@ -3,12 +3,8 @@ import Toggles from '@gio-design/components/es/components/toggles';
 import '@gio-design/components/es/components/toggles/style/index.less';
 
 export default () => {
-  const status = {
-    a: true,
-  };
   const change = (val: any) => {
     console.log('change-test', val);
-    console.log('fhsihfisd', status);
   };
-  return <Toggles onChange={change} defaultChecked={status.a} />;
+  return <Toggles onChange={change} />;
 };
