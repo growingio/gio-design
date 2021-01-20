29d9f52175526d26a86ccecc0238205691d65aaf
lihang <lihang@growingio.com>
2021-01-20 11:40:46 +0800

    style(pagination): fix style error

--- src/components/pagination/Pagination.stories.tsx
+++ src/components/pagination/Pagination.stories.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import Pagination, { PaginationProps } from './index'
-import './style'
+import Pagination, { PaginationProps } from './index';
+import './style';
 
 export default {
-    title: 'Components/Functional/Pagination',
-    component: Pagination,
+  title: 'Components/Functional/Pagination',
+  component: Pagination,
 } as Meta;
 
-const Template : Story<PaginationProps> = (args) => <Pagination {...args} />;
+const Template: Story<PaginationProps> = (args) => <Pagination {...args} />;
 export const Default = Template.bind({});
 Default.args = {
-    total: 100,
-}
+  total: 120,
+  showQuickJumper: true,
+  disabled: false,
+  pageSize: 10,
+  defaultCurrent: 1,
+  hideOnSinglePage: false,
+  showSizeChanger: true,
+};
