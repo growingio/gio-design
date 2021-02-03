0a57fc027bc49679589fadf48307018b86f93f75
tongchuliu <tongchuliu@growingio.com>
2021-02-03 14:39:58 +0800

    docs(link): add stories and document for link component
    docs(link): add stories and document for link component

--- src/components/link/Link.stories.tsx
+++ src/components/link/Link.stories.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react/types-6-0'
-
-import Link from './index'
-import { ILinkProps } from './interface'
-import './style'
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Docs from './Link.mdx';
+import Link from './index';
+import { ILinkProps } from './interface';
+import './style';
 
 export default {
-    title: 'Components/Basic/Link',
-    component: Link,
+  title: 'Basic Components/Link',
+  component: Link,
+  parameters: {
+    docs: {
+      page: Docs,
+    },
+  },
 } as Meta;
 
-const Template : Story<ILinkProps> = (args) => <Link {...args}>GrowingIO</Link>
+const Template: Story<ILinkProps> = (args) => <Link {...args}>GrowingIO</Link>;
 
 export const Default = Template.bind({});
 Default.args = {
-    to: "https://www.growingio.com",
-}
+  to: 'https://www.growingio.com',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  to: 'https://www.growingio.com',
+  disabled: true,
+};
+export const CustomComponent = Template.bind({});
+CustomComponent.args = {
+  to: 'https://www.growingio.com',
+  component: 'span',
+};
