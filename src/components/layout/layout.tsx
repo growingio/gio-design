5ab8417324dfd5ccb5814e14a23eeb049bd49abf
Lee Hon <lihang@growingio.com>
2021-02-20 11:25:43 +0800

    feat(layout): add fixed prop (#790)
    support fixed navigation
    
    Co-authored-by: Jack <panjie@growingio.com>

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef, createContext } from 'react';
+import React, { useLayoutEffect, useRef, createContext, useEffect, useContext } from 'react';
 import classNames from 'classnames';
 import { useWindowSize, useSetState } from 'react-use';
 import { LayoutProps, LayoutState, ContentState, LayoutContextType } from './interfaces';
@@ -8,7 +8,7 @@ import Content from './content';
 import Sider from './sider';
 import useSiders from './useSiders';
 
-const initLayoutState = { wide: false };
+const initLayoutState = { wide: false, fixed: false };
 const initContentState = { maxWidth: 0, margin: 0 };
 
 export const LayoutContext = createContext<LayoutContextType>({
@@ -16,23 +16,33 @@ export const LayoutContext = createContext<LayoutContextType>({
   contentState: initContentState,
 } as LayoutContextType);
 
-const Layout = ({ prefixCls: customizePrefixCls, className, style, children }: LayoutProps) => {
+const Layout = ({ prefixCls: customizePrefixCls, fixed, className, style, children }: LayoutProps) => {
   const { width } = useWindowSize();
   const containerRef = useRef<HTMLDivElement>(null);
   const prefixCls = usePrefixCls('layout', customizePrefixCls);
   const [localLayoutState, setLayoutState] = useSetState<LayoutState>(initLayoutState);
   const [localContentState, setContentState] = useSetState<ContentState>(initContentState);
   const [siders, sidersWidth, removeSider, updateSiders, margin] = useSiders();
+  const { layoutState: parentLayoutState } = useContext(LayoutContext);
 
   useLayoutEffect(() => {
     const layoutWidth = containerRef.current?.getBoundingClientRect().width ?? 0;
     setLayoutState(() => {
-      return { wide: layoutWidth > localContentState.maxWidth + 2 * localContentState.margin + sidersWidth };
+      return {
+        wide:
+          localContentState.maxWidth !== 0 &&
+          layoutWidth > localContentState.maxWidth + 2 * localContentState.margin + sidersWidth,
+      };
     });
   }, [width, setLayoutState, localContentState, siders, sidersWidth]);
 
+  useEffect(() => {
+    setLayoutState({ fixed });
+  }, [fixed, setLayoutState]);
+
   const mergedStyle = {
     ...{ marginLeft: margin[0], marginRight: margin[1] },
+    ...(parentLayoutState.fixed ? { height: 'calc(100% - 60px)' } : {}),
     ...style,
   };
 
@@ -51,6 +61,7 @@ const Layout = ({ prefixCls: customizePrefixCls, className, style, children }: L
         ref={containerRef}
         className={classNames(prefixCls, className, {
           [`${prefixCls}-has-sider`]: siders.length > 0,
+          [`${prefixCls}-fixed`]: fixed,
         })}
         style={mergedStyle}
       >
