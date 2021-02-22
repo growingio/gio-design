05565fc6746475636cb9cb6df30f033013d02f38
tongchuliu <tongchuliu@growingio.com>
2021-02-22 15:33:30 +0800

    docs(progress): add stories and document for progress component

--- src/components/progress/Progress.stories.tsx
+++ src/components/progress/Progress.stories.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-
+import { withDesign } from 'storybook-addon-designs';
+import Docs from './Progress.mdx';
 import { ProgressProps } from './interface';
 import Progress from './index';
 import './style';
 
 export default {
-  title: 'Components/Basic/Progress',
+  title: 'Basic Components/Progress',
   component: Progress,
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/kP3A6S2fLUGVVMBgDuUx0f/GrowingIO-Design-Components?node-id=889%3A1311',
+      allowFullscreen: true,
+    },
+    docs: {
+      page: Docs,
+    },
+  },
 } as Meta;
 
-const Template: Story<ProgressProps> = (args) => <Progress {...args} />;
+const Template: Story<ProgressProps> = (args) => (
+  <div>
+    <Progress {...args} percent={100} status="success" />
+    <Progress {...args} percent={30} status="exception" />
+    <Progress {...args} />
+  </div>
+);
 
 export const Default = Template.bind({});
 Default.args = {
-  percent: 30,
+  percent: 60,
+  status: "active",
 };
