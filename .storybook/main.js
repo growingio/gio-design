419a1dfe0b4bd9b9a4ef5a61278e2e8f562c3ae5
Pan Jie <panjie@growingio.com>
2021-04-06 11:28:57 +0800

    chore: upgrade storybook and husky

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     {
       name: '@storybook/addon-docs',
@@ -24,6 +23,10 @@ module.exports = {
     'storybook-addon-designs',
     path.resolve('./.storybook/footer/preset'),
   ],
+  features: {
+    postcss: false,
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.less$/,
