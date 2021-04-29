4580512359969347798f10185a0343d467f212dc
lihang <lihang@growingio.com>
2021-04-29 19:30:35 +0800

    replace loading

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -44,7 +44,21 @@ const Loading = forwardRef<HTMLDivElement, LoadingProps>((props, ref) => {
       return <span className={`${prefixCls}-indicator`}>{indicator}</span>;
     }
     return (
-      <div className={`${prefixCls}-ring`} />
+      <div className={`${prefixCls}-ring`}>
+        {[1, 2, 3, 4].map((item) => (
+          <div className={`${prefixCls}-ring-line ${prefixCls}-ring-line-${item}`}>
+            <div className={`${prefixCls}-ring-line-cog`}>
+                <div className={`${prefixCls}-ring-line-cog-inner ${prefixCls}-ring-line-cog-inner-left`} />
+            </div>
+            <div className={`${prefixCls}-ring-line-ticker`}>
+                <div className={`${prefixCls}-ring-line-cog-inner ${prefixCls}-ring-line-cog-inner-center`} />
+            </div>
+            <div className={`${prefixCls}-ring-line-cog`}>
+                <div className={`${prefixCls}-ring-line-cog-inner ${prefixCls}-ring-line-cog-inner-right`} />
+            </div>
+          </div>
+        ))}
+      </div>
     );
   }, [prefixCls, indicator]);
 
