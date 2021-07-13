7e74903a9094d602a486f191bdcece06c091d888
GrowingIO <growingio@MacBookdeMacBook-Pro.local>
2021-07-13 17:16:47 +0800

    test(dropdown): rewrite the test

---
+++ src/components/dropdown/__test__/Dropdown.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Default, Placement } from '../Dropdown.stories';
+import Dropdown from '../index';
+import Button from '../../button';
+
+describe('Testing dropdown', () => {
+  it('default dropdown', () => {
+    render(<Default {...Default.args} />);
+    fireEvent.click(screen.getByRole('button', { name: /更多功能/i }));
+    expect(screen.getByText('用户属性')).toBeTruthy();
+  });
+
+  it('placement dropdown', () => {
+    render(<Placement {...Placement.args} />);
+    fireEvent.click(screen.getAllByRole('button')[0]);
+    expect(screen.getAllByRole('tooltip')).toHaveLength(1);
+  });
+
+  it('function overlay', () => {
+    const overlays = () => <div id="overlay-content">我是下拉内容</div>;
+    render(
+      <Dropdown overlay={overlays()} trigger="hover">
+        <Button>点击</Button>
+      </Dropdown>
+    );
+    fireEvent.mouseEnter(screen.getByRole('button'));
+    fireEvent.click(screen.getByText('我是下拉内容'));
+    expect(screen.getByText('我是下拉内容')).toBeTruthy();
+  });
+});
