780d6b4e56c648252ef3ed29461d59e069a130bd
GrowingIO <growingio@MacBookdeMacBook-Pro.local>
2021-07-14 09:44:35 +0800

    test(grid): rewrite the test

--- src/components/grid/__test__/grid.test.tsx
+++ src/components/grid/__test__/grid.test.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Row, Col } from '..';
+import { render, screen } from '@testing-library/react';
+import Grid, { Row, Col } from '..';
+import { Default } from '../Grid.stories';
 
 describe('Testing Grid', () => {
+  it('default grid', () => {
+    render(<Default {...Default.args} />);
+    expect(screen.getAllByText('25%')).toHaveLength(4);
+  });
+
   it('should be stable', () => {
-    const { asFragment } = render(
+    render(
       <Row gutter={[10, 10]}>
         <Col span={3}>0</Col>
         <Col order={1}>3</Col>
         <Col>1</Col>
         <Col span={7}>
-          <Row>
+          <Row gutter={undefined}>
             <Col span={6} offset={3}>
               4
             </Col>
@@ -19,6 +25,18 @@ describe('Testing Grid', () => {
         </Col>
       </Row>
     );
-    expect(asFragment()).toMatchSnapshot();
+    expect(screen.getAllByText(1)).toHaveLength(1);
+  });
+
+  it('empty grid', () => {
+    render(<Grid />);
+    expect(screen.queryByText(/^./)).toBeNull();
+  });
+
+  it('should render nested', () => {
+    const { container } = render(
+      <Grid span={12} gap={1} direction="row" wrap="wrap" justify="center" alignItems="center" alignContent="center" />
+    );
+    expect(container.getElementsByClassName('gio-grid')).not.toBeNull();
   });
 });
