5741e8d89943aaed63007752a6ffe19fe4bb4cdf
WORLDI <66404428+WORLDI@users.noreply.github.com>
2021-07-19 13:20:15 +0800

    test(popover): rewrite the test (#1108)
    Co-authored-by: GrowingIO <growingio@MacBookdeMacBook-Pro.local>

--- src/components/popover/index.tsx
+++ src/components/popover/index.tsx
@@ -1,33 +1,4 @@
-import React from 'react';
-import Tooltip from '../tooltip';
-import getPlacements from '../tooltip/placements';
-import { PopoverProps } from './interface';
-import usePrefixCls from '../../utils/hooks/use-prefix-cls';
-
-export { PopoverProps } from './interface';
-
-const Popover: React.FC<PopoverProps> = (props: PopoverProps) => {
-  const { children, contentArea, footerArea, prefixCls: customizePrefixCls, subPrefixCls = 'popover', arrowPointAtCenter, autoAdjustOverflow, ...rest } = props;
-  const prefixCls = usePrefixCls(subPrefixCls, customizePrefixCls);
-
-  const popoverOverlay = () => (
-    <>
-      {contentArea && <div className={`${prefixCls}-inner-content`}>{contentArea}</div>}
-      {footerArea && <div className={`${prefixCls}-inner-footer`}>{footerArea}</div>}
-    </>
-  );
-  return (
-    <Tooltip 
-      prefixCls={customizePrefixCls} 
-      subPrefixCls={subPrefixCls} 
-      overlay={popoverOverlay()} 
-      builtinPlacements={getPlacements({ arrowPointAtCenter, autoAdjustOverflow, arrowWidth: 12})} 
-      arrowPointAtCenter={arrowPointAtCenter} autoAdjustOverflow={autoAdjustOverflow}
-      {...rest} 
-    >
-      {children}
-    </Tooltip>
-  );
-};
+import Popover from './Popover';
 
 export default Popover;
+export { PopoverProps } from './interface';
