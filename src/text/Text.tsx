9d131580c7e5cc8c08923d7693d5b3af9dacdeb3
hejiang <hejiang@growingio.com>
2021-08-03 14:37:21 +0800

    fix(text): fix the text error

--- src/text/Text.tsx
+++ src/text/Text.tsx
@@ -7,7 +7,7 @@ import usePrefixCls from '../utils/hooks/use-prefix-cls';
 import './style';
 
 const Text: React.FC<TextProps> = (props: TextProps) => {
-  const [regWidth, setRegWidth] = useState<number>(0);
+  const [truncated, setTruncted] = useState(false);
   const {
     className,
     style,
@@ -15,28 +15,42 @@ const Text: React.FC<TextProps> = (props: TextProps) => {
     width = 0,
     ellipsis = '...',
     children,
-    trimwhitespace = false,
+    trimwhitespace,
     placement = 'top',
   } = props;
 
-  const contentRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<any>(null);
+  const contentRef = useRef<any>(null);
   const prefixCls = usePrefixCls('text');
   const cls = classnames(prefixCls, className);
 
+  const [currentWidth, setCurrentWidth] = useState(width);
+
+  const handleTruncate = (isTruncated: boolean) => {
+    setCurrentWidth(containerRef.current.children[0].offsetWidth * lines);
+    if (isTruncated !== truncated) {
+      setTruncted(isTruncated);
+      isTruncated && contentRef.current.onResize();
+    }
+  };
+
   useEffect(() => {
-    setRegWidth(contentRef.current?.clientWidth as number);
-  }, [lines, width]);
+    setCurrentWidth(containerRef.current.children[0].offsetWidth * lines);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <Tooltip placement={placement} title={children}>
-      <div className={cls} style={style} ref={contentRef}>
+    <Tooltip placement={placement} title={children} disabled={!truncated}>
+      <div ref={containerRef} className={cls} style={style}>
         <Truncate
           lines={lines}
-          width={(lines > 1) ? (regWidth * lines) : width}
+          width={lines > 1 ? currentWidth : width}
           ellipsis={ellipsis}
           trimWhitespace={trimwhitespace}
+          onTruncate={handleTruncate}
+          ref={contentRef}
         >
-          {children}
+          {children?.replace(/\s/g, '')}
         </Truncate>
       </div>
     </Tooltip>
