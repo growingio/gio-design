d006be53b2e24898d987392a6ea63f901fbdf3d3
dingweijing <growingio@Ding-Weijing.local>
2021-09-30 13:27:31 +0800

    feat(space): modify space component

--- src/space/__test__/Space.test.tsx
+++ src/space/__test__/Space.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Default } from '../Space.stories';
+import { Default } from '../demos/Space.stories';
 import Space from '../index';
 
 describe('Testing space', () => {
@@ -10,8 +10,30 @@ describe('Testing space', () => {
   });
 
   it('empty space', () => {
-    const { container } = render(<Space size={10} autoWrap />);
+    const { container } = render(<Space size={10} autoWrap align="center" />);
     expect(container.getElementsByClassName('gio-space')).toHaveLength(0);
+  });
+
+  it('render chlidren', () => {
+    const { container } = render(
+      <Space size={[1, 2]} direction="horizontal" split="|">
+        <span>点击</span>
+        {undefined}
+        {123}
+      </Space>
+    );
+    expect(container.getElementsByClassName('gio-space-item')).toHaveLength(2);
+  });
+
+  it('empty split', () => {
+    const { container } = render(
+      <Space direction="vertical" split="">
+        <span>点击</span>
+        {undefined}
+        {123}
+      </Space>
+    );
+    expect(container.getElementsByClassName('gio-space-item')).toHaveLength(2);
   });
 
   it('not render undefined chlidren', () => {
