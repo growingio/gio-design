327bd539bde4b2281d2a72da147a4e5899fc399f
dingweijing <growingio@Ding-Weijing.local>
2021-09-30 13:27:31 +0800

    feat(space): modify space component

--- src/space/demos/Space.stories.tsx
+++ src/space/demos/Space.stories.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { withDesign } from 'storybook-addon-designs';
 import { PlusCircleFilled } from '@gio-design/icons';
-import Space, { SpaceProps } from './index';
-import Docs from './Space.mdx';
+import Space, { SpaceProps } from '../index';
+import Docs from './SpacePage';
 import { Button } from '../..';
 
 export default {
-  title: 'Basic Components/Space',
+  title: 'Layout/Space',
   component: Space,
   decorators: [withDesign],
   parameters: {
@@ -21,9 +21,10 @@ const Template: Story<SpaceProps> = (args) => (
   <Space {...args}>
     <Button>主要按钮</Button>
     <Button type="secondary">次要按钮</Button>
-    <Button type="secondary" icon={<PlusCircleFilled />}>次要按钮</Button>
+    <Button type="secondary" icon={<PlusCircleFilled />}>
+      次要按钮
+    </Button>
   </Space>
 );
 
 export const Default = Template.bind({});
-
