33f02f46539ed81e1af78bd190ae28cd24dba0bd
hejiang <hejiang@growingio.com>
2021-10-08 14:16:31 +0800

    feat(layout): modify layout component

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export { default as Drawer, DrawerProps } from './components/drawer';
 export { default as Dropdown, DropdownProps } from './components/dropdown';
 export { default as Empty, EmptyProps } from './components/empty';
 export { default as Input, InputProps, InputNumberProps, TextAreaProps } from './components/input';
-export { default as Layout, LayoutProps } from './components/layout';
+export { default as Layout, LayoutProps } from './layout';
 export { default as Link, LinkProps } from './components/link';
 export { default as List, ListProps } from './components/list';
 export { default as ListPicker, ListPickerProps } from './list-picker';
