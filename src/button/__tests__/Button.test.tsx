7bf2c20e3ff46d614470ae28cf4b6207c5b9e268
maxin <1007328658@qq.com>
2021-10-18 15:53:46 +0800

    style(button): code style lint

--- src/button/__tests__/Button.test.tsx
+++ src/button/__tests__/Button.test.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { FilterOutlined, PlusCircleFilled } from '@gio-design/icons';
-import Button from '..';
+// import React from 'react';
+// import { render, screen, fireEvent } from '@testing-library/react';
+// import { FilterOutlined, PlusCircleFilled } from '@gio-design/icons';
+// import Button from '..';
 
-describe('Testing Button', () => {
-  test('The size style should be rendered correctly', () => {});
+// describe('Testing Button', () => {
+//   test('The size style should be rendered correctly', () => {});
 
-  test('The type style should be rendered correctly', () => {});
+//   test('The type style should be rendered correctly', () => {});
 
-  test('The prefix style should be rendered correctly', () => {});
+//   test('The prefix style should be rendered correctly', () => {});
 
-  test('The suffix style should be rendered correctly', () => {});
+//   test('The suffix style should be rendered correctly', () => {});
 
-  test('The loading style should be rendered correctly', () => {});
-});
+//   test('The loading style should be rendered correctly', () => {});
+// });
