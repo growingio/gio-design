9ad4c9e0420c91b16e029fb460ae453f97f7809b
maxin <48519459+nnmax@users.noreply.github.com>
2021-10-25 17:37:40 +0800

    refactor(breadcrumb): refactor the breadcrumb component (#1384)
    * refactor(breadcrumb): refactor the breadcrumb component
    
    * refactor(breadcrumb): move the old breadcrumb to src/legacy
    
    * docs(breadcrumb): add the stories of the breadcrumb component

--- src/breadcrumb/demos/Breadcrumb.stories.tsx
+++ src/breadcrumb/demos/Breadcrumb.stories.tsx
@@ -1,52 +1,83 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { withDesign } from 'storybook-addon-designs';
-import Docs from './BreadcrumbPage';
-import Breadcrumb from '../index';
-import { BreadcrumbProps } from '../interface';
+import { HomeOutlined, RightDoubleOutlined, ShareOutlined, TagOutlined } from '@gio-design/icons';
+import Breadcrumb, { BreadcrumbProps } from '..';
+import Link from '../../link';
 import '../style';
 
 export default {
-  title: 'Components/Breadcrumb',
+  title: 'Upgraded/Breadcrumb',
   component: Breadcrumb,
-  decorators: [withDesign],
-  parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/kP3A6S2fLUGVVMBgDuUx0f/GrowingIO-Design-Components?node-id=1108%3A3874',
-      allowFullscreen: true,
-    },
-    docs: {
-      page: Docs,
-    },
-  },
-  argTypes: {
-    separator: {
-      control: { type: 'text' },
-    },
-  },
 } as Meta;
 
-const Template: Story<BreadcrumbProps> = (args) => <Breadcrumb {...args} />;
+const Template: Story<BreadcrumbProps> = (args) => (
+  <>
+    <h3>简单的面包屑</h3>
+    <Breadcrumb {...args}>
+      <Link href="/">主页</Link>
+      <Link href="https://www.growingio.com">二级导航</Link>
+      <Link aria-current="page" href="/?path=/story/upgraded-breadcrumb--default">
+        当前页面
+      </Link>
+    </Breadcrumb>
+  </>
+);
 
 export const Default = Template.bind({});
 Default.args = {
-  routes: [
-    {
-      path: '?path=/story/basic-components-breadcrumb--default',
-      breadcrumbName: '首页',
-    },
-    {
-      path: '#',
-      breadcrumbName: '一级面包屑',
-    },
-    {
-      path: '##',
-      breadcrumbName: '二级面包屑',
-    },
-    {
-      path: '###',
-      breadcrumbName: '三级面包屑',
-    },
-  ],
+  separator: '/',
 };
+
+const IconTemplate: Story<BreadcrumbProps> = (args) => (
+  <>
+    <h3>带图标的面包屑</h3>
+    <Breadcrumb {...args}>
+      <Link prefix={<HomeOutlined />} href="/">
+        主页
+      </Link>
+      <Link prefix={<ShareOutlined />} href="https://www.growingio.com">
+        二级导航
+      </Link>
+      <Link prefix={<TagOutlined />} aria-current="page" href="/?path=/story/upgraded-breadcrumb--default">
+        当前页面
+      </Link>
+    </Breadcrumb>
+  </>
+);
+
+export const IconBreadcrumb = IconTemplate.bind({});
+IconBreadcrumb.args = {
+  separator: '/',
+};
+
+const CustomSeparatorTemplate: Story<BreadcrumbProps> = (args) => (
+  <>
+    <h3>自定义分隔符</h3>
+    <Breadcrumb separator=">" {...args}>
+      <Link href="/">主页</Link>
+      <Link href="https://www.growingio.com">二级导航</Link>
+      <Link aria-current="page" href="/?path=/story/upgraded-breadcrumb--default">
+        当前页面
+      </Link>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb separator="-" {...args}>
+      <Link href="/">主页</Link>
+      <Link href="https://www.growingio.com">二级导航</Link>
+      <Link aria-current="page" href="/?path=/story/upgraded-breadcrumb--default">
+        当前页面
+      </Link>
+    </Breadcrumb>
+    <br />
+    <Breadcrumb separator={<RightDoubleOutlined size="14px" />} {...args}>
+      <Link href="/">主页</Link>
+      <Link href="https://www.growingio.com">二级导航</Link>
+      <Link aria-current="page" href="/?path=/story/upgraded-breadcrumb--default">
+        当前页面
+      </Link>
+    </Breadcrumb>
+  </>
+);
+
+export const CustomSeparator = CustomSeparatorTemplate.bind({});
+CustomSeparator.args = {};
