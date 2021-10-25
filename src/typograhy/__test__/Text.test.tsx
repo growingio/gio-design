8dd73072c53ad03408ef2e6b345a2145f45d1519
Jack Pan <i@jackpan.me>
2021-10-25 14:52:36 +0800

    fix(text): render nothing for empty text

--- src/typograhy/__test__/Text.test.tsx
+++ src/typograhy/__test__/Text.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Styles } from '../demos/Text.stories';
+import { Empty, Styles } from '../demos/Text.stories';
 
 describe('Text', () => {
   it('show with different styles', () => {
     render(<Styles />);
     expect(screen.queryByText('...')).toBeNull();
+  });
+
+  it('render nothing with empty text', () => {
+    const { container } = render(<Empty {...Empty.args} />);
+    expect(container.querySelector('.gio-text')).toBeNull();
   });
 });
