8c42027f33d7b1f5139748e9809bdabb0db90a64
ZhaoChen <ittisennsinn@gmail.com>
2021-10-27 15:49:00 +0800

    fix(checkbox, toggle, radio, switch): fix prefixCls & style

--- src/toggle/Toggles.tsx
+++ src/toggle/Toggles.tsx
@@ -10,7 +10,7 @@ import WithRef from '../utils/withRef';
 
 const InnerToggle: React.ForwardRefRenderFunction<HTMLInputElement, TogglesProps> = (props, ref) => {
   const { defaultOn = false, on, disabled, className, checkedChildren, uncheckedChildren, onChange } = props;
-  const prefixCls = usePrefixCls('toggles');
+  const prefixCls = usePrefixCls('toggle-new');
 
   const [status, setStatus] = useControlledState<boolean>(on, defaultOn);
 
