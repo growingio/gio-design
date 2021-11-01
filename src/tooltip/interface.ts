a1e3406d774c0a647a26258f06c6c45498ede4c4
Zhang Rui <zhangrui@growingio.com>
2021-11-01 15:16:13 +0800

    fix(interface): update the interface

--- src/tooltip/interface.ts
+++ src/tooltip/interface.ts
@@ -9,8 +9,8 @@ export interface TooltipLink {
 }
 
 export interface TooltipProps extends Omit<PopoverProps, 'content'> {
-  overlay?: string | React.ReactNode | ReactRender;
-  title?: string | React.ReactNode | ReactRender;
+  overlay?: React.ReactNode;
+  title?: React.ReactNode;
 
   subPrefixCls?: string;
 
