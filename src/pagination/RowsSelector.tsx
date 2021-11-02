269efbce09df28f5e3e10f7836d17b798279630e
maxin <48519459+nnmax@users.noreply.github.com>
2021-11-02 16:31:10 +0800

    refactor(pagination): refactor the dom structure of Pagination (#1417)
    * fix(button): make the Button style more robust
    
    * refactor(pagination): refactor the dom structure of Pagination
    
    Co-authored-by: maxin <maxin@growingio.com>

---
+++ src/pagination/RowsSelector.tsx
@@ -0,0 +1,49 @@
+import React, { useContext, useRef } from 'react';
+import { Select } from '..';
+import { PaginationContext } from './Pagination';
+
+const RowsSelector: React.FC<{
+  /**
+   * 每页行数改变的回调
+   * @param rows 改变后的行数
+   */
+  onRowsChange?: (rows: number) => void;
+
+  'aria-label'?: string;
+}> = (props) => {
+  const { 'aria-label': ariaLabel, onRowsChange } = props;
+
+  const { defaultPageSize, onPageSizeChange, pageSize, pageSizeOptions, prefixCls } = useContext(PaginationContext);
+
+  const previousPageSizeRef = useRef<number>(pageSize || defaultPageSize);
+
+  return (
+    <div aria-label={ariaLabel} className={`${prefixCls}__rows`}>
+      <p>每页展示</p>
+      <Select
+        defaultValue={defaultPageSize}
+        value={pageSize}
+        size="small"
+        className={`${prefixCls}__rows__select`}
+        getContainer={(node) => node.parentElement || document.body}
+        autoWidth
+        options={pageSizeOptions.map((rowSize) => {
+          const value = Number.parseInt(`${rowSize}`, 10);
+          return {
+            label: value,
+            value,
+          };
+        })}
+        onSelect={(value) => {
+          const currentPageSize = Number.parseInt(`${value}`, 10);
+          onRowsChange?.(currentPageSize);
+          onPageSizeChange?.(currentPageSize, previousPageSizeRef.current);
+          previousPageSizeRef.current = currentPageSize;
+        }}
+      />
+      <p>条</p>
+    </div>
+  );
+};
+
+export default RowsSelector;
