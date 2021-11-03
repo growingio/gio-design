cddbb844cd9ba016603acb248e8a72ad1f363536
Zhang Rui <zhangrui@growingio.com>
2021-11-03 16:06:39 +0800

    feat(Loading): add new Loading component

---
+++ src/loading/Wheel.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import usePrefixCls from '../utils/hooks/use-prefix-cls';
+import { WheelProps } from './interface';
+
+const Wheel: React.FC<WheelProps> = (props: WheelProps) => {
+  const { indicator, prefixCls: customizePrefixCls } = props;
+  const prefixCls = usePrefixCls('loading-new', customizePrefixCls);
+  if (indicator) {
+    return <span className={`${prefixCls}-indicator`}>{indicator}</span>;
+  }
+  return (
+    <div className={`${prefixCls}-ring`}>
+      {[1, 2, 3, 4].map((item) => (
+        <div className={`${prefixCls}-ring-line ${prefixCls}-ring-line-${item}`}>
+          <div className={`${prefixCls}-ring-line-cog`}>
+            <div className={`${prefixCls}-ring-line-cog-inner ${prefixCls}-ring-line-cog-inner-left`} />
+          </div>
+          <div className={`${prefixCls}-ring-line-ticker`}>
+            <div className={`${prefixCls}-ring-line-cog-inner ${prefixCls}-ring-line-cog-inner-center`} />
+          </div>
+          <div className={`${prefixCls}-ring-line-cog`}>
+            <div className={`${prefixCls}-ring-line-cog-inner ${prefixCls}-ring-line-cog-inner-right`} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Wheel;
