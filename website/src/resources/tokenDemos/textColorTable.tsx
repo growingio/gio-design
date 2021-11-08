fc963e7bcd6ec751c66405b5c69a478b1d655335
Jack Pan <i@jackpan.me>
2021-11-08 19:54:37 +0800

    chore: remove website folder

--- website/src/resources/tokenDemos/textColorTable.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import TokenTable from './tokenTable';
-import { ExampleType } from './interface';
-import { textColorExample } from './examples';
-
-export default () => <TokenTable prefix="ColorText" example={textColorExample as ExampleType} />;
