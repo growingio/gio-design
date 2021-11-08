0d118eaea2352e5016f103219a8ca1b256811e43
Jack Pan <i@jackpan.me>
2021-11-08 19:49:04 +0800

    chore: remove website folder

--- website/src/resources/tokenDemos/fontSizeTable.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import TokenTable from './tokenTable';
-import { ExampleType } from './interface';
-import { fontSizeExample } from './examples';
-
-export default () => <TokenTable prefix="SizeFont" example={fontSizeExample as ExampleType} />;
