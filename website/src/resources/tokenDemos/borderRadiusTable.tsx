6750f7b5767d95153d83df87a1a7553ad6b9dc42
Jack Pan <i@jackpan.me>
2021-11-08 19:49:04 +0800

    chore: remove website folder

--- website/src/resources/tokenDemos/borderRadiusTable.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import TokenTable from './tokenTable';
-import { ExampleType } from './interface';
-import { borderRadiusExample } from './examples';
-
-export default () => <TokenTable prefix="RadiusBorder" example={borderRadiusExample as ExampleType} />;
