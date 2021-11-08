c60d0f86b5e09f6fbf8afef0e89ae07a17bd7622
Zhou <zhouzhaoting@growingio.com>
2021-11-08 16:24:37 +0800

    modify babel

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,9 @@ module.exports = {
   presets: ['@babel/preset-react', ['@babel/preset-env'], '@babel/preset-typescript'],
   plugins: [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-transform-modules-commonjs',
-    '@babel/transform-runtime',
     ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     ['@babel/plugin-proposal-private-methods', { loose: true }],
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/transform-runtime',
   ],
 };
