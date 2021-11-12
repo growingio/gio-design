3dfca86137fdd6c7cc9ba524e145e5e624dcd0e0
zhaoting zhou <zhouz1224@gmail.com>
2021-11-12 12:31:30 +0800

    fix(scripts): fix build (#1459)

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   presets: ['@babel/preset-react', ['@babel/preset-env'], '@babel/preset-typescript'],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    '@babel/plugin-transform-modules-commonjs',
-    '@babel/transform-runtime',
-  ],
+  plugins: ['@babel/plugin-transform-modules-commonjs', '@babel/transform-runtime'],
 };
