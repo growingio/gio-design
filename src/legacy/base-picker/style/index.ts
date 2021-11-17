86ccb3ea237167ca945fb4fbc0b06def8d7735e1
maxin <maxin@growingio.com>
2021-11-17 17:19:28 +0800

    fix(base-picker): modify style deps

--- src/legacy/base-picker/style/index.ts
+++ src/legacy/base-picker/style/index.ts
@@ -1,10 +1,5 @@
-import '../../empty-prompt/style/index.less';
 import '../../alert/style';
-import '../../button/style';
-import '../../input/style';
 import '../../search-bar/style';
-import '../../loading/style';
-import '../../tabs/style';
-import '../../cascader/style';
-import '../../dropdown/style';
+import '../../tab-nav';
+import '../../list';
 import './index.less';
