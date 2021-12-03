5377a137d9516655f0d1db0881f5cc4abf3faff1
Jack Pan <i@jackpan.me>
2021-12-03 14:34:13 +0800

    chore: update the changelog file when releasing

---
+++ .releaserc.js
@@ -0,0 +1,8 @@
+module.export = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+  ],
+};
