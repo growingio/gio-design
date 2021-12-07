fa2d8dccefd0bbf1cb94717a175bbee96f77f014
Jack Pan <i@jackpan.me>
2021-12-07 15:56:20 +0800

    chore: update semantic-release config

--- release.config.js
+++ release.config.js
@@ -2,8 +2,21 @@ module.export = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
     '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} changelog\n\n${nextRelease.notes}',
+      },
+    ],
     '@semantic-release/github',
   ],
 };
