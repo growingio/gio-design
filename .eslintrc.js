fb6f58ad0637715f653bebfff0566ee027bdd6ba
maxin <maxin@growingio.com>
2022-01-04 17:54:58 +0800

    fix(switch): fix style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
-  extends: ["airbnb", "airbnb/hooks", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     mocha: true,
-    jest: true
+    jest: true,
   },
   // 忽略对全局 variable 的检查
   globals: {
-    Promise: true
+    Promise: true,
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   plugins: ['prettier', '@typescript-eslint'],
   // Rewrite style
@@ -24,24 +30,39 @@ module.exports = {
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': [1],
     '@typescript-eslint/no-unused-vars': 'error',
-    'react/jsx-filename-extension': ['error', {
-      extensions: ['.js', '.jsx', '.ts', '.tsx']
-    }],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
     'react/jsx-props-no-spreading': 'off',
     'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: true
-    }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
     'import/extensions': ['off', 'never'],
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
     'no-underscore-dangle': 'off',
     'no-unused-expressions': [0],
     'react/static-property-placement': ['error', 'static public field'],
-    '@typescript-eslint/no-unused-expressions': ['error', {
-      allowShortCircuit: true
-    }],
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+      },
+    ],
     '@typescript-eslint/explicit-module-boundary-types': ['off'],
-    'react/jsx-no-bind': ['off']
-  }
+    'react/jsx-no-bind': ['off'],
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        assert: 'either',
+      },
+    ],
+  },
 };
