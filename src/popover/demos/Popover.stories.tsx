ee271d450a768143b03e9de6501386f3fc9d0068
Ryan Zhang <eneblue@126.com>
2022-01-07 17:36:01 +0800

    fix(Popover): use max height to set popover position (#1778)
    * fix(Popover): design position issues
    
    * fix(popover): height
    
    * fix(Popover): update Popover height offset
    
    * fix(Popover): resolve eslint issue
    
    Co-authored-by: Zhang Rui <zhangrui@growingio.com>

--- src/popover/demos/Popover.stories.tsx
+++ src/popover/demos/Popover.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import Popover from '../Popover';
 import { PopoverProps } from '../interface';
@@ -291,4 +291,53 @@ const DisabledTemplate: Story<PopoverProps> = (args) => (
 export const Disabled = DisabledTemplate.bind({});
 Disabled.args = {
   content,
+};
+
+const ScrollTemplate: Story<PopoverProps> = (args) => {
+  const divRef = useRef();
+  const [divHeight, setDivHeight] = useState(1000);
+  const [marginHeight, setMarginHeight] = useState(400);
+  return (
+    <div>
+      <div>
+        外框高度
+        <Input value={divHeight} style={{ margin: 4 }} onChange={(e) => setDivHeight(Number(e.target.value))} />
+        间距高度
+        <Input value={marginHeight} style={{ margin: 4 }} onChange={(e) => setMarginHeight(Number(e.target.value))} />
+      </div>
+      <div ref={divRef} style={{ height: divHeight, width: 400, backgroundColor: '#eeeeee' }}>
+        <Popover {...args} strategy="fixed" trigger="click" getContainer={() => divRef.current}>
+          <Button style={{ margin: `${marginHeight}px 0 0 100px` }}>Show Popover</Button>
+        </Popover>
+      </div>
+    </div>
+  );
+};
+export const ScrollPopover = ScrollTemplate.bind({});
+ScrollPopover.args = {
+  content: (
+    <div>
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试！
+      <br />
+      这是一个测试!
+    </div>
+  ),
 };
--- src/popover/usePop.ts
+++ src/popover/usePop.ts
@@ -36,6 +36,18 @@ const clear3D = (transform: string): string | number[] => {
   return transform;
 };
 
+const getMaxHeight = (element: HTMLElement, maxHeight = 0): number => {
+  if (!element) {
+    return maxHeight;
+  }
+  const elHeight = element.offsetHeight;
+  const newMax = elHeight > maxHeight ? elHeight : maxHeight;
+  if (element.parentElement) {
+    return getMaxHeight(element.parentElement, newMax);
+  }
+  return newMax;
+};
+
 const usePop = ({ referenceElement, popperElement, placement, modifiers, strategy }: UsePopProps) => {
   const { styles, attributes, ...popperProps } = usePopper(referenceElement, popperElement, {
     placement: placements[placement],
@@ -47,13 +59,15 @@ const usePop = ({ referenceElement, popperElement, placement, modifiers, strateg
     if (Array.isArray(three)) {
       const [x, y, z] = three;
       const divHeight = popperElement.offsetHeight;
-      const winHeight = window.innerHeight;
+      const winHeight = getMaxHeight(referenceElement);
+
       if (styles?.popper?.bottom === 'auto') {
         let yField = y < 0 ? 0 : y;
         yField = yField + divHeight > winHeight ? winHeight - divHeight : yField;
         styles.popper.transform = `translate3d(${x}px, ${yField}px, ${z || 0}px)`;
       } else if (styles?.popper?.bottom === '0') {
-        const yField = y + divHeight > 0 ? 0 : y;
+        const maxYField = winHeight - (window.pageYOffset + window.innerHeight);
+        const yField = y + divHeight > maxYField ? maxYField : y;
         styles.popper.transform = `translate3d(${x}px, ${yField}px, ${z || 0}px)`;
       }
     }
