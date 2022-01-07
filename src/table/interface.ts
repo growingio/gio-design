128f2ff4a0515e014e9fe1cc69cd20870be812cd
maxin <48519459+nnmax@users.noreply.github.com>
2022-01-07 19:28:28 +0800

    revert(table): revert "perf(table): optimize `Table` (#1774)" (#1780)
    This reverts commit cb32a9bc2da0c2193b60e2398cfb53a2c804655a.
    
    Co-authored-by: maxin <maxin@growingio.com>

--- src/table/interface.ts
+++ src/table/interface.ts
@@ -105,11 +105,11 @@ interface FilterState<RecordType> {
   isControlled: boolean;
 }
 
-interface TitleProps<RecordType>
-  extends Pick<ColumnType<RecordType>, 'title' | 'info' | 'align' | 'filterSearchPlaceHolder' | 'sortDirections'> {
+interface TitleProps<RecordType> {
   prefixCls: string;
   sorterState?: SortState<RecordType>;
   filterState?: FilterState<RecordType>;
+  column: ColumnType<RecordType>;
   updateSorterStates: (sortState: SortState<RecordType>) => SortState<RecordType>;
   updateFilterStates: (filterState: FilterState<RecordType>) => Record<string, string[]>;
   onTriggerStateUpdate: (onTriggerStateUpdateProps?: OnTriggerStateUpdateProps<RecordType>) => void;
---
+++ src/table/utils.ts
@@ -0,0 +1,27 @@
+import { clone, has, get, join, set } from 'lodash';
+import { ColumnsType } from './interface';
+
+export const TABLE_PREFIX_CLS = 'table';
+
+// eslint-disable-next-line import/prefer-default-export
+export const translateInnerColumns = <RecordType>(
+  columns: ColumnsType<RecordType> | undefined
+): ColumnsType<RecordType> =>
+  clone<ColumnsType<RecordType> | undefined>(columns)?.map((cloneColumn) => {
+    if (!has(cloneColumn, 'key')) {
+      if (has(cloneColumn, 'dataIndex')) {
+        if (Array.isArray(get(cloneColumn, 'dataIndex'))) {
+          set(cloneColumn, 'key', join(get(cloneColumn, 'dataIndex'), '-'));
+        } else {
+          set(cloneColumn, 'key', get(cloneColumn, 'dataIndex'));
+        }
+      } else {
+        // eslint-disable-next-line no-console
+        console.warn('gio-design table: column key or dataIndex must have one');
+      }
+    }
+    if (has(cloneColumn, 'children')) {
+      set(cloneColumn, 'children', translateInnerColumns(get(cloneColumn, 'children')));
+    }
+    return cloneColumn;
+  }) || [];
