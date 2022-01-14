870a9b0be5717db625c42b9d13087d5ebd495d63
maxin <48519459+nnmax@users.noreply.github.com>
2022-01-14 10:30:07 +0800

    fix(search-bar, property-selector): add deps style (#1797)
    Co-authored-by: maxin <maxin@growingio.com>

--- src/legacy/property-selector/style/index.ts
+++ src/legacy/property-selector/style/index.ts
@@ -2,7 +2,7 @@ import '../../base-picker/style';
 import '../components/list/style';
 import '../../../popover/style'; // new
 import '../../selector-pro/style';
-import '../../../card/style'; // new
+import '../../card/style/index';
 import '../../../loading/style'; // new
 import '../../../tag/style'; // new
 import '../../../page/style'; // new
