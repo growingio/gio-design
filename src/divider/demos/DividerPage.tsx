ef9354fee56fcd31bdc40c8ef9301da3c544ba73
maxin <48519459+nnmax@users.noreply.github.com>
2022-02-22 16:58:56 +0800

    docs(link, divider): add stories of link and divider (#1824)
    * docs(link): add stories of link
    
    * docs(divider): add stories of divider
    
    Co-authored-by: maxin <maxin@growingio.com>

--- src/divider/demos/DividerPage.tsx
+++ src/divider/demos/DividerPage.tsx
@@ -1,48 +1,45 @@
 import React from 'react';
-import { Canvas, Title, Heading, Story, Subheading, ArgsTable } from '@storybook/addon-docs';
+import { Canvas, Title, Heading, Story, Subheading, ArgsTable, Description } from '@storybook/addon-docs';
 import { useIntl } from 'react-intl';
 import Divider from '../Divider';
+import { Alert } from '../..';
 
 export default function ListPage() {
   const { formatMessage } = useIntl();
 
   return (
     <>
-      <Title>{formatMessage({ defaultMessage: 'Divider  分割线' })}</Title>
-      <p>
-        {formatMessage({
-          defaultMessage: '区隔内容的横竖分割线。',
-        })}
-      </p>
-      <p>
-        <a href="https://www.figma.com/file/lLYusioN7e9ifkQnIXeT4G/GIO-Design-(Running-File)?node-id=4066%3A43358">
-          Figma
-        </a>
-      </p>
-      <p>Upgrading Guide</p>
-      <ul>
-        <li>新增组件</li>
-        <li>APi: orientation参数控制分割线方向，flexItem参数兼容flex布局高度</li>
-      </ul>
+      <Title>{formatMessage({ defaultMessage: 'Divider 分割线' })}</Title>
+      <Description>{formatMessage({ defaultMessage: '分隔线是对列表和布局中的内容进行分组的一条细线。' })}</Description>
 
       <Heading>{formatMessage({ defaultMessage: '代码演示' })}</Heading>
-      <Subheading>{formatMessage({ defaultMessage: '样例展示' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-divider--demo" />
-      </Canvas>
+
       <Subheading>{formatMessage({ defaultMessage: '列表分割线' })}</Subheading>
       <Canvas>
-        <Story id="upgraded-divider--horizontal" />
+        <Story id="upgraded-divider--list-divider" />
       </Canvas>
 
-      <Subheading>{formatMessage({ defaultMessage: '行内元素的垂直分割线' })}</Subheading>
+      <Subheading>{formatMessage({ defaultMessage: '垂直分割线' })}</Subheading>
+      <Description>
+        {formatMessage({ defaultMessage: '您也可以使用 `orientation="vertical"` 属性将分割线渲染成垂直形状。' })}
+      </Description>
+
+      <Alert
+        type="warning"
+        message={formatMessage(
+          {
+            defaultMessage:
+              '请注意这其中使用了 <code>flexItem</code> 属性来适应 flex 容器。如果容器设置了 <code>display: flex | inline-flex</code> 布局，需要设置 <code>flexItem</code> 属性，否则不能正确计算分割线的高度。',
+          },
+          {
+            code: (text) => <code>{text}</code>,
+          }
+        )}
+      />
       <Canvas>
-        <Story id="upgraded-divider--vertical" />
+        <Story id="upgraded-divider--vertical-divider" />
       </Canvas>
-      <Subheading>{formatMessage({ defaultMessage: '适应 Flex 容器的垂直分割线' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-divider--flex-item" />
-      </Canvas>
+
       <Heading>{formatMessage({ defaultMessage: '参数说明' })}</Heading>
       <ArgsTable of={Divider} />
     </>
