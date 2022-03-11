c539c3e8347fcb6265a390bc89748ed12d4a7aba
maxin <maxin@growingio.com>
2022-03-11 11:52:02 +0800

    docs(alert): add story docs

--- src/alert/demos/AlertPage.tsx
+++ src/alert/demos/AlertPage.tsx
@@ -1,57 +1,68 @@
 import React from 'react';
-import { Canvas, Title, Heading, Story, Subheading, ArgsTable } from '@storybook/addon-docs';
+import { Canvas, Title, Heading, Story, Subheading, ArgsTable, Description, Subtitle } from '@storybook/addon-docs';
 import { useIntl } from 'react-intl';
+import { Figma } from 'storybook-addon-designs/blocks';
 import Alert from '../Alert';
 
-export default function ListPage() {
+export default function AlertPage() {
   const { formatMessage } = useIntl();
 
   return (
     <>
       <Title>{formatMessage({ defaultMessage: 'Alert 警告信息' })}</Title>
-      <p>
-        当某个页面需要向用户显示警告、提示的信息时使用，这部分信息是用户必须了解的，例如未对公众号进行授权则影响某些功能的使用。
-      </p>
-      <p>通常为页面级提示信息，提示重要性高，通常位置在页面或弹窗顶部。</p>
-      <p> Alert宽度根据不同使用场景，可以设置为固定宽度，内容超长时，换行展示。</p>
-      <p>
-        <a href="https://www.figma.com/file/lLYusioN7e9ifkQnIXeT4G/GIO-Design-(Running-File)?node-id=4093%3A45313">
-          Figma
-        </a>
-      </p>
-      <p>Upgrading Guide</p>
-      <ul>
-        <li>样式变化：4种颜色样式变化，边框，对齐问题，样式更新。</li>
-        <li>API变化：size大小控制去除，texticon自定义关闭按钮去除。</li>
-      </ul>
+      <Description>
+        {formatMessage({
+          defaultMessage: '当某个页面需要向用户显示警告、提示的信息时使用，这部分信息是用户必须了解的。',
+        })}
+      </Description>
+      <Subtitle>{formatMessage({ defaultMessage: '使用场景' })}</Subtitle>
+      <Description>
+        {formatMessage({ defaultMessage: '通常为页面级提示信息，提示重要性高，通常位置在页面或弹窗顶部。' })}
+      </Description>
+      <Subtitle>{formatMessage({ defaultMessage: '设计稿' })}</Subtitle>
+      <Figma
+        height="50%"
+        collapsable
+        url="https://www.figma.com/file/kP3A6S2fLUGVVMBgDuUx0f/GIO-Design?node-id=4093%3A45313"
+      />
+
       <Heading>{formatMessage({ defaultMessage: '代码演示' })}</Heading>
-      <Subheading>{formatMessage({ defaultMessage: 'Demo' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-alert--demo" />
-      </Canvas>
-      <Subheading>{formatMessage({ defaultMessage: 'default' })}</Subheading>
+
+      <Subheading>{formatMessage({ defaultMessage: '简单的警告提示' })}</Subheading>
+      <Description>
+        {formatMessage({ defaultMessage: '`Alert` 组件有四种不同程度的级别，每种都有自己独特的颜色。' })}
+      </Description>
       <Canvas>
         <Story id="upgraded-alert--default" />
       </Canvas>
 
-      <Subheading>{formatMessage({ defaultMessage: 'Icon & 标题 & 关闭' })}</Subheading>
+      <Subheading>{formatMessage({ defaultMessage: '描述信息' })}</Subheading>
+      <Description>
+        {formatMessage({ defaultMessage: '您可以使用 `description` 属性在标题下方显示描述信息。' })}
+      </Description>
       <Canvas>
-        <Story id="upgraded-alert--no-description" />
+        <Story id="upgraded-alert--description" />
       </Canvas>
 
-      <Subheading>{formatMessage({ defaultMessage: 'Icon & 正文 & 关闭' })}</Subheading>
+      <Subheading>{formatMessage({ defaultMessage: '图标' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: '您可以设置 `showIcon` 属性来显示默认的图标。您也可以设置 `icon` 属性，覆盖默认的图标。',
+        })}
+      </Description>
       <Canvas>
-        <Story id="upgraded-alert--no-title" />
+        <Story id="upgraded-alert--icons" />
       </Canvas>
 
-      <Subheading>{formatMessage({ defaultMessage: '标题 & 正文 & 关闭' })}</Subheading>
+      <Subheading>{formatMessage({ defaultMessage: '可关闭的' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage:
+            '您可以设置 `closeable` 属性，在 Alert 组件的右侧展示一个关闭按钮，点击后会隐藏 Alert 组件，同时触发 `onClose` 事件。',
+        })}
+      </Description>
       <Canvas>
-        <Story id="upgraded-alert--no-icon" />
-      </Canvas>
-
-      <Subheading>{formatMessage({ defaultMessage: 'Icon & 标题 & 正文 ' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-alert--no-close" />
+        <Story id="upgraded-alert--closeable" />
       </Canvas>
 
       <Heading>{formatMessage({ defaultMessage: '参数说明' })}</Heading>
