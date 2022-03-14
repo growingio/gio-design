4976b2aa4bb5e23b3e416237072d1bf3df29259b
maxin <maxin@growingio.com>
2022-03-14 16:55:47 +0800

    docs(checkbox): add story

--- src/checkbox/demos/CheckboxPage.tsx
+++ src/checkbox/demos/CheckboxPage.tsx
@@ -1,49 +1,72 @@
 import React from 'react';
-import { Canvas, Title, Heading, Story, Subheading, ArgsTable } from '@storybook/addon-docs';
+import { Canvas, Title, Heading, Story, Subheading, ArgsTable, Description, Subtitle } from '@storybook/addon-docs';
 import { useIntl } from 'react-intl';
+import { Figma } from 'storybook-addon-designs/blocks';
 import Checkbox from '../Checkbox';
 
-export default function ListPage() {
+export default function CheckboxPage() {
   const { formatMessage } = useIntl();
 
   return (
     <>
       <Title>{formatMessage({ defaultMessage: 'Checkbox 多选框' })}</Title>
-      <p>
+
+      <Description>
         {formatMessage({
-          defaultMessage: '在一组可选项中进行多项选择:',
+          defaultMessage: '在一组可选项中进行多项选择。',
         })}
-      </p>
-      <p>
-        <a href="https://www.figma.com/file/lLYusioN7e9ifkQnIXeT4G/GIO-Design-(Running-File)?node-id=4078%3A37260">
-          Figma
-        </a>
-      </p>
-      <p>Upgrading Guide</p>
-      <ul>
-        <li>样式变化：无变动</li>
-        <li>APi变化：无变动</li>
-        <li> checkbox和switch区别，checkbox 支持多选</li>
-      </ul>
+      </Description>
+
+      <Subtitle>{formatMessage({ defaultMessage: '设计稿' })}</Subtitle>
+      <Figma
+        height="50%"
+        collapsable
+        url="https://www.figma.com/file/kP3A6S2fLUGVVMBgDuUx0f/GIO-Design?node-id=4078%3A37260"
+      />
 
       <Heading>{formatMessage({ defaultMessage: '代码演示' })}</Heading>
-      <Subheading>{formatMessage({ defaultMessage: '样例展示' })}</Subheading>
+
+      <Subheading>{formatMessage({ defaultMessage: '基础的多选框' })}</Subheading>
       <Canvas>
-        <Story id="upgraded-checkbox--demo" />
-      </Canvas>
-      <Subheading>{formatMessage({ defaultMessage: '基本样式' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-checkbox--default" />
+        <Story id="upgraded-checkbox--basic" />
       </Canvas>
 
-      <Subheading>{formatMessage({ defaultMessage: '分组' })}</Subheading>
+      <Subheading>{formatMessage({ defaultMessage: '标签' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: '使用 `children`，您可以为 `Checkbox` 设置一个标签。',
+        })}
+      </Description>
+      <Canvas>
+        <Story id="upgraded-checkbox--labels" />
+      </Canvas>
+
+      <Subheading>{formatMessage({ defaultMessage: '颜色' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage:
+            '使用 `color` 属性，您可以为 `Checkbox` 设置 `checked` 或 `indeterminate` 状态下的背景填充色。',
+        })}
+      </Description>
+      <Canvas>
+        <Story id="upgraded-checkbox--color" />
+      </Canvas>
+
+      <Subheading>{formatMessage({ defaultMessage: '受控的 Checkbox' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: '您可以使用 `checked` 和 `onChange` 属性控制 `Checkbox`。',
+        })}
+      </Description>
+      <Canvas>
+        <Story id="upgraded-checkbox--controlled" />
+      </Canvas>
+
+      <Subheading>{formatMessage({ defaultMessage: '多选框组' })}</Subheading>
       <Canvas>
         <Story id="upgraded-checkbox--group" />
       </Canvas>
-      <Subheading>{formatMessage({ defaultMessage: '不定态' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-checkbox--indeterminate" />
-      </Canvas>
+
       <Heading>{formatMessage({ defaultMessage: '参数说明' })}</Heading>
       <ArgsTable of={Checkbox} />
     </>
