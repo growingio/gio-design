f1fad4f9bbf28eff7cdfc5a8a05ae933c0ae3bd3
hiker90 <DREAMSONTHEWAY@163.COM>
2022-03-25 19:00:48 +0800

    fix svg id has colon error (#1928)
    Co-authored-by: YanHui <yanhui@growingio.com>

--- src/page/svg/EmptyImageSVG.tsx
+++ src/page/svg/EmptyImageSVG.tsx
@@ -4,7 +4,7 @@ import { InjectRandomIds } from '.';
 const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElement>>((props, ref) => (
   <svg role="img" aria-labelledby="title" viewBox="0 0 300 193" fill="none" ref={ref} {...props}>
     <title id="title">Dashboard image</title>
-    <rect x={59} y={14.118} width={182.432} height={121.622} rx={4} fill="url(#prefix__paint0_linear)" />
+    <rect x={59} y={14.118} width={182.432} height={121.622} rx={4} fill="url(#empty_prefix_paint0_linear)" />
     <g opacity={0.8}>
       <rect x={71.28} y={29.32} width={62.331} height={41.047} rx={4} fill="#fff" />
       <rect
@@ -14,7 +14,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         height={4.561}
         rx={2.28}
         transform="rotate(90 98.644 33.88)"
-        fill="url(#prefix__paint1_linear)"
+        fill="url(#empty_prefix_paint1_linear)"
       />
       <rect
         x={86.482}
@@ -23,7 +23,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         height={4.561}
         rx={2.28}
         transform="rotate(90 86.482 41.482)"
-        fill="url(#prefix__paint2_linear)"
+        fill="url(#empty_prefix_paint2_linear)"
       />
       <rect
         x={110.807}
@@ -32,7 +32,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         height={4.561}
         rx={2.28}
         transform="rotate(90 110.807 49.083)"
-        fill="url(#prefix__paint3_linear)"
+        fill="url(#empty_prefix_paint3_linear)"
       />
       <rect
         x={122.969}
@@ -41,7 +41,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         height={4.561}
         rx={2.28}
         transform="rotate(90 122.969 41.482)"
-        fill="url(#prefix__paint4_linear)"
+        fill="url(#empty_prefix_paint4_linear)"
       />
     </g>
     <g opacity={0.8}>
@@ -50,7 +50,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         fillRule="evenodd"
         clipRule="evenodd"
         d="M212.623 76.606l-32.345.06 16.519-28.735a30.557 30.557 0 00-14.881-3.81c-16.955.03-30.675 13.801-30.644 30.757.032 16.955 13.802 30.675 30.757 30.644 16.338-.03 29.671-12.816 30.594-28.916z"
-        fill="url(#prefix__paint5_linear)"
+        fill="url(#empty_prefix_paint5_linear)"
       />
       <path
         opacity={0.8}
@@ -65,15 +65,15 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
       <rect x={71.28} y={79.489} width={62.331} height={41.047} rx={4} fill="#fff" />
       <path
         d="M86.814 87.743l-9.453 24.446a.5.5 0 00.467.68h48.274c.531 0 .696-.717.218-.949l-22.462-10.917a.507.507 0 00-.309-.042l-10.66 1.966a.5.5 0 01-.569-.344l-4.562-14.807a.5.5 0 00-.944-.033z"
-        fill="url(#prefix__paint6_linear)"
+        fill="url(#empty_prefix_paint6_linear)"
       />
     </g>
-    <path d="M0 192.568c118.691-46.234 184.527-45.657 300 0H0z" fill="url(#prefix__paint7_linear)" />
+    <path d="M0 192.568c118.691-46.234 184.527-45.657 300 0H0z" fill="url(#empty_prefix_paint7_linear)" />
     <path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M93.055 141.417a13.717 13.717 0 007.356-12.157c0-7.577-6.142-13.719-13.72-13.719-7.576 0-13.718 6.142-13.718 13.719 0 6.712 4.82 12.299 11.19 13.486-1.605 4.88-3.424 9.491-5.036 13.578-2.574 6.525-4.62 11.713-4.426 14.522.41 5.939 20.306 5.008 25.247 0 3.817-3.869-.706-17.07-6.893-29.429z"
-      fill="url(#prefix__paint8_linear)"
+      fill="url(#empty_prefix_paint8_linear)"
     />
     <circle cx={241.216} cy={14.189} r={14.189} fill="#88A6FF" />
     <rect x={231.756} y={12.612} width={18.919} height={3.153} rx={1.577} fill="#fff" />
@@ -97,7 +97,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
     />
     <defs>
       <linearGradient
-        id="prefix__paint0_linear"
+        id="empty_prefix_paint0_linear"
         x1={149.882}
         y1={174.231}
         x2={149.882}
@@ -108,7 +108,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         <stop offset={1} stopColor="#F7F8FC" />
       </linearGradient>
       <linearGradient
-        id="prefix__paint1_linear"
+        id="empty_prefix_paint1_linear"
         x1={127.023}
         y1={38.441}
         x2={98.645}
@@ -119,7 +119,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         <stop offset={1} stopColor="#B8D1FF" />
       </linearGradient>
       <linearGradient
-        id="prefix__paint2_linear"
+        id="empty_prefix_paint2_linear"
         x1={109.794}
         y1={46.043}
         x2={87.496}
@@ -130,7 +130,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         <stop offset={1} stopColor="#B8D1FF" />
       </linearGradient>
       <linearGradient
-        id="prefix__paint3_linear"
+        id="empty_prefix_paint3_linear"
         x1={126.01}
         y1={53.644}
         x2={111.821}
@@ -141,7 +141,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         <stop offset={1} stopColor="#B8D1FF" />
       </linearGradient>
       <linearGradient
-        id="prefix__paint4_linear"
+        id="empty_prefix_paint4_linear"
         x1={146.28}
         y1={46.043}
         x2={122.969}
@@ -152,7 +152,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         <stop offset={1} stopColor="#B8D1FF" />
       </linearGradient>
       <linearGradient
-        id="prefix__paint5_linear"
+        id="empty_prefix_paint5_linear"
         x1={140.769}
         y1={95.77}
         x2={207.115}
@@ -163,7 +163,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         <stop offset={1} stopColor="#B8D1FF" stopOpacity={0.9} />
       </linearGradient>
       <linearGradient
-        id="prefix__paint6_linear"
+        id="empty_prefix_paint6_linear"
         x1={102.687}
         y1={86.399}
         x2={102.687}
@@ -174,7 +174,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         <stop offset={1} stopColor="#B8D1FF" />
       </linearGradient>
       <linearGradient
-        id="prefix__paint7_linear"
+        id="empty_prefix_paint7_linear"
         x1={151.21}
         y1={158.113}
         x2={151.21}
@@ -186,7 +186,7 @@ const EmptyImage = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElemen
         <stop offset={1} stopColor="#fff" />
       </linearGradient>
       <linearGradient
-        id="prefix__paint8_linear"
+        id="empty_prefix_paint8_linear"
         x1={107.307}
         y1={115.385}
         x2={72.692}
