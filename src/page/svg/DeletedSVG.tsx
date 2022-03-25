702f4b64e5ccb13f4f5999fe3dddc27e173f3fd6
YanHui <yanhui@growingio.com>
2022-03-25 17:16:15 +0800

    fix svg id has colon error

--- src/page/svg/DeletedSVG.tsx
+++ src/page/svg/DeletedSVG.tsx
@@ -3,10 +3,10 @@ import { InjectRandomIds } from '.';
 
 const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElement>>((props, ref) => (
   <svg viewBox="0 0 450 288" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}>
-    <path d="M0 273.292C178.037 216.941 276.79 217.644 450 273.292H0Z" fill="url(#paint0_linear_7150:7006)" />
+    <path d="M0 273.292C178.037 216.941 276.79 217.644 450 273.292H0Z" fill="url(#delete_prefix_paint0_linear)" />
     <path
       d="M329 10C399 38.1373 379 200 247 223C115.001 246 78.3701 191.892 69.685 145.446C61 99.0004 94 55.9998 155 39.9999C216.001 23.9999 259.001 -18.1373 329 10Z"
-      fill="url(#paint1_linear_7150:7006)"
+      fill="url(#delete_prefix_paint1_linear)"
     />
     <path
       opacity="0.1"
@@ -19,12 +19,12 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
     />
     <path
       d="M144.231 239.923L226.385 281.769V162.077L137.154 130.077L144.231 239.923Z"
-      fill="url(#paint2_linear_7150:7006)"
+      fill="url(#delete_prefix_paint2_linear)"
     />
-    <path d="M310.692 131L307.923 237.769L226.692 282.077V162.077L310.692 131Z" fill="url(#paint3_linear_7150:7006)" />
+    <path d="M310.692 131L307.923 237.769L226.692 282.077V162.077L310.692 131Z" fill="url(#delete_prefix_paint3_linear)" />
     <path
       d="M311 130.692L224.846 103.923L136.538 130.692L227 162.384L311 130.692Z"
-      fill="url(#paint4_linear_7150:7006)"
+      fill="url(#delete_prefix_paint4_linear)"
     />
     <g opacity="0.1">
       <path
@@ -42,7 +42,7 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
     </g>
     <path
       d="M176.231 196.846L226.692 162.385L136.846 130.385L87 161.462L176.231 196.846Z"
-      fill="url(#paint5_linear_7150:7006)"
+      fill="url(#delete_prefix_paint5_linear)"
     />
     <g opacity="0.3">
       <path
@@ -53,15 +53,15 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
     </g>
     <path
       d="M363 159.616L312.231 131.308L226.692 162.385L276.231 198.077L363 159.616Z"
-      fill="url(#paint6_linear_7150:7006)"
+      fill="url(#delete_prefix_paint6_linear)"
     />
     <path
       d="M189.769 71.6156L225.154 104.231L137.154 130.693L100.231 93.7695L189.769 71.6156Z"
-      fill="url(#paint7_linear_7150:7006)"
+      fill="url(#delete_prefix_paint7_linear)"
     />
     <path
       d="M258.692 71L347.615 92.2308L311.308 130.692L225.769 104.231L258.692 71Z"
-      fill="url(#paint8_linear_7150:7006)"
+      fill="url(#delete_prefix_paint8_linear)"
     />
     <g opacity="0.2">
       <path
@@ -72,7 +72,7 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
     </g>
     <defs>
       <linearGradient
-        id="paint0_linear_7150:7006"
+        id="delete_prefix_paint0_linear"
         x1="226.815"
         y1="231.298"
         x2="226.815"
@@ -83,12 +83,12 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
         <stop offset="0.9999" stopColor="white" stopOpacity="0.453125" />
         <stop offset="1" stopColor="white" />
       </linearGradient>
-      <linearGradient id="paint1_linear_7150:7006" x1="245" y1="28" x2="249" y2="202" gradientUnits="userSpaceOnUse">
+      <linearGradient id="delete_prefix_paint1_linear" x1="245" y1="28" x2="249" y2="202" gradientUnits="userSpaceOnUse">
         <stop stopColor="#F8FAFF" />
         <stop offset="1" stopColor="#E7F0FF" />
       </linearGradient>
       <linearGradient
-        id="paint2_linear_7150:7006"
+        id="delete_prefix_paint2_linear"
         x1="140.5"
         y1="196"
         x2="226"
@@ -98,12 +98,12 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
         <stop stopColor="#94AFFF" />
         <stop offset="1" stopColor="#ABC0FF" stopOpacity="0" />
       </linearGradient>
-      <linearGradient id="paint3_linear_7150:7006" x1="227" y1="210.5" x2="311" y2="203" gradientUnits="userSpaceOnUse">
+      <linearGradient id="delete_prefix_paint3_linear" x1="227" y1="210.5" x2="311" y2="203" gradientUnits="userSpaceOnUse">
         <stop stopColor="#BDCEFF" />
         <stop offset="1" stopColor="#9CB5FF" />
       </linearGradient>
       <linearGradient
-        id="paint4_linear_7150:7006"
+        id="delete_prefix_paint4_linear"
         x1="223.769"
         y1="103.923"
         x2="226.5"
@@ -113,12 +113,12 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
         <stop stopColor="#AEC3FF" />
         <stop offset="1" stopColor="#648CFF" />
       </linearGradient>
-      <linearGradient id="paint5_linear_7150:7006" x1="181" y1="144" x2="138" y2="189" gradientUnits="userSpaceOnUse">
+      <linearGradient id="delete_prefix_paint5_linear" x1="181" y1="144" x2="138" y2="189" gradientUnits="userSpaceOnUse">
         <stop stopColor="#D6E4FF" />
         <stop offset="1" stopColor="#C3D8FF" />
       </linearGradient>
       <linearGradient
-        id="paint6_linear_7150:7006"
+        id="delete_prefix_paint6_linear"
         x1="241.5"
         y1="161"
         x2="343.5"
@@ -129,7 +129,7 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
         <stop offset="1" stopColor="#AAC8FF" />
       </linearGradient>
       <linearGradient
-        id="paint7_linear_7150:7006"
+        id="delete_prefix_paint7_linear"
         x1="127"
         y1="93.5004"
         x2="218.5"
@@ -139,7 +139,7 @@ const ForbiddenSVG = React.forwardRef<SVGSVGElement, React.SVGAttributes<SVGElem
         <stop stopColor="#C4D8FF" />
         <stop offset="1" stopColor="#ADCAFF" />
       </linearGradient>
-      <linearGradient id="paint8_linear_7150:7006" x1="307" y1="84" x2="274" y2="115.5" gradientUnits="userSpaceOnUse">
+      <linearGradient id="delete_prefix_paint8_linear" x1="307" y1="84" x2="274" y2="115.5" gradientUnits="userSpaceOnUse">
         <stop stopColor="#B5CFFF" />
         <stop offset="1" stopColor="#CADDFF" />
       </linearGradient>
