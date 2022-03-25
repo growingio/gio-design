ba50e9229334ff3664eec4b7d4927a3691fee8b2
haozhigang <haozhigang@outlook.com>
2022-03-25 20:23:45 +0800

    docs(tabs): add some demos and docs

--- src/tabs/demos/TabsPage.tsx
+++ src/tabs/demos/TabsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Canvas, Title, Heading, Story, Subheading, ArgsTable } from '@storybook/addon-docs';
+import { Canvas, Title, Heading, Story, Subheading, ArgsTable, Description, Subtitle } from '@storybook/addon-docs';
 import { useIntl } from 'react-intl';
+import { Figma } from 'storybook-addon-designs/blocks';
 import Tabs from '../Tabs';
 
 export default function ListPage() {
@@ -9,33 +10,49 @@ export default function ListPage() {
   return (
     <>
       <Title>{formatMessage({ defaultMessage: 'Tabs 标签页' })}</Title>
-      <p>
+      <Description>
+        {formatMessage({
+          defaultMessage: '选项卡切换组件。',
+        })}
+      </Description>
+      <Subtitle>
+        {formatMessage({
+          defaultMessage: '使用场景',
+        })}
+      </Subtitle>
+      <Description>
         {formatMessage({
           defaultMessage: '标签栏使您能够通过单个对象结构关联多个导航端点，tab 用于切换下方的内容',
         })}
-      </p>
-      <p>
-        <a href="https://www.figma.com/file/lLYusioN7e9ifkQnIXeT4G/GIO-Design-(Running-File)?node-id=4092%3A41170">
-          Figma
-        </a>
-      </p>
-      <p>Upgrading Guide</p>
-      <ul>
-        <li>样式：全新颜色样式</li>
-        <li>子元素为tab</li>
-        <li>不再区分tabnav，tabs，移除了tabnav</li>
-        <li>block参数取消</li>
-        <li>去掉了过渡动画</li>
-      </ul>
+      </Description>
+      <Description>
+        {formatMessage({
+          defaultMessage: 'Tabs 和 Switch 的区别在于，Tabs 是切换下方内容控制内容联动，多用于导航，Switch 只是切换选项，并不能控制内容联动。 ',
+        })}
+      </Description>
+
+      <Subtitle>
+        {formatMessage({
+          defaultMessage: '设计稿',
+        })}
+      </Subtitle>
+      <Figma
+        height="50%"
+        collapsable
+        url="https://www.figma.com/file/kP3A6S2fLUGVVMBgDuUx0f/GIO-Design?node-id=6826%3A77724"
+      />
       <Heading>{formatMessage({ defaultMessage: '代码演示' })}</Heading>
-      <Subheading>{formatMessage({ defaultMessage: '样例展示' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-tabs--demo" />
-      </Canvas>
-      <Subheading>{formatMessage({ defaultMessage: 'default' })}</Subheading>
+
+      <Subheading>{formatMessage({ defaultMessage: '基本' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: 'Tabs 默认选中第一项。 Switch 的区别在于，Tabs 是切换下方内容控制内容联动，多用于导航，Switch 只是切换选项，并不能控制内容联动。 ',
+        })}
+      </Description>
       <Canvas>
         <Story id="upgraded-tabs--default" />
       </Canvas>
+
       <Subheading>{formatMessage({ defaultMessage: 'no-tab' })}</Subheading>
       <Canvas>
         <Story id="upgraded-tabs--no-tab" />
