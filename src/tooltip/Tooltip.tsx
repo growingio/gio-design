5279bf51faf6e3d8b16b16e4c6c5257affa1aa0b
Ryan Zhang <eneblue@126.com>
2022-04-06 19:17:05 +0800

    fix(hideDelay): update hideDelay (#1948)
    * fix(hideDelay): update hideDelay
    
    * fix(hideDelay): default value is 0
    
    Co-authored-by: Zhang Rui <zhangrui@growingio.com>

--- src/tooltip/Tooltip.tsx
+++ src/tooltip/Tooltip.tsx
@@ -30,6 +30,7 @@ const Tooltip = (props: TooltipProps) => {
     subPrefixCls = 'tooltip',
     overlayClassName,
     overlayInnerClassName,
+    hideDelay,
     ...rest
   } = props;
   const prefixCls = usePrefixCls(subPrefixCls, customizePrefixCls);
@@ -98,11 +99,10 @@ const Tooltip = (props: TooltipProps) => {
     }
     return element;
   }
-
   return (
     <Popover
       {...rest}
-      hideDelay={tooltipLink?.link ? 300 : 0}
+      hideDelay={tooltipLink?.link && !hideDelay ? 300 : hideDelay || 0}
       allowArrow={allowArrow}
       prefixCls={customizePrefixCls}
       overlayClassName={classNames(prefixCls, overlayClassName)}
