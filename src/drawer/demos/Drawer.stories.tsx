0e845464729d688e774c917b083aedc77919f9e2
vermilionAnd <zhujiahong@growingio>
2022-04-12 11:18:18 +0800

    docs(drawer): add drawer storybook

--- src/drawer/demos/Drawer.stories.tsx
+++ src/drawer/demos/Drawer.stories.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { action } from '@storybook/addon-actions';
+import { EditOutlined } from '@gio-design/icons';
 import Drawer from '../index';
 import Button from '../../button';
 import { DrawerProps } from '../interfaces';
 import Docs from './DrawerPage';
 import '../style';
+import Divider from '../../divider';
+import { Card } from '../../card';
+import Skeleton from '../../skeleton';
 
 export default {
   title: 'Upgraded/Drawer',
@@ -21,6 +26,7 @@ export default {
   },
 } as Meta;
 
+
 const Template: Story<DrawerProps> = (args) => {
   const [visible, setVisible] = useState(false);
   return (
@@ -30,41 +36,124 @@ const Template: Story<DrawerProps> = (args) => {
         {...args}
         data-testid="drawer"
         visible={visible}
-        onClose={() => {
+        onClose={(e) => {
           setVisible(false);
+          action('onClose')
+          console.log(e,'onClose')
         }}
-      >
-        <>宽度自动撑开 宽度自动撑开 宽度自动撑开 宽度自动撑开 宽度自动撑开</>
-      </Drawer>
+       />
     </div>
   );
 };
-export const AdaptiveWidthDemo = Template.bind({});
-AdaptiveWidthDemo.args = {
+
+export const Default = Template.bind({});
+Default.args={
+  children: 'content'
+}
+
+export const Adaptive = Template.bind({});
+Adaptive.args = {
   title: '标题',
   size: 'normal',
+  children: '宽度由内容决定'.repeat(5)
 };
 
-const FixedTemplate: Story<DrawerProps> = (args) => {
+export const Fixed = Template.bind({});
+Fixed.args = {
+  title: '标题',
+  size: 'fixed',
+  children: '固定宽度'.repeat(10)
+};
+
+export const CustomWidth = Template.bind({});
+CustomWidth.args = {
+  title: '自定义宽度',
+  width: 300,
+  size: 'normal'
+};
+
+export const OnCloseAndAfterClose = Template.bind({});
+OnCloseAndAfterClose.args = {
+  title: '关闭后回调',
+  afterClose: (e: any)=> console.log(e,'afterClose')
+};
+
+export const Footer = Template.bind({});
+Footer.args = {
+  title: '带有footer的drawer',
+  footer: <><Button style={{width:'100%'}} >Footer</Button></>,
+  afterClose: ()=>action('afterClose')
+};
+
+export const Mask = Template.bind({});
+Mask.args = {
+  title: 'mask蒙板去除',
+  mask:false,
+};
+
+export const Closable = Template.bind({});
+Closable.args = {
+  title: '无关闭按钮',
+  closable:false,
+};
+
+export const CloseIcon = Template.bind({});
+CloseIcon.args = {
+  closeIcon:<EditOutlined />,
+};
+
+export const MaskStyleOrBodyStyle = Template.bind({});
+MaskStyleOrBodyStyle.args = {
+  title: 'maskStyle change',
+  maskStyle: {
+    background:'gray',
+  },
+  bodyStyle: {
+    background:'orange',
+  }
+};
+
+export const Demo:Story<DrawerProps> = (args) => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
       <Button onClick={() => setVisible(true)}>Open Drawer</Button>
       <Drawer
-        {...args}
+        title='抽屉标题'
+        size='fixed'
+        data-testid="drawer"
         visible={visible}
         onClose={() => {
           setVisible(false);
+          action('onClose')
         }}
-      >
-        宽度固定500 宽度固定500 宽度固定500 宽度固定500 宽度固定500 宽度固定500 宽度固定500 宽度固定500 宽度固定500
-      </Drawer>
+        footer={
+          <div>
+            <Button type='secondary'>取消</Button>
+            &nbsp;
+            <Button>确定</Button>
+          </div>
+        }
+       >
+
+          <Card>
+            <span style={{
+              display:'flex',
+              height:32,
+              width:'100%',
+              justifyContent:'space-between',
+            }}>
+              <span style={{fontSize:'24px'}}>标题</span>
+              <Button  type='secondary' prefix={<EditOutlined />}>编辑 </Button>
+            </span>
+            <Divider  />
+            <Skeleton
+              paragraph={{
+                row: 5
+              }}
+            />
+          </Card>
+       </Drawer>
     </div>
   );
-};
-
-export const fixedWidthDemo = FixedTemplate.bind({});
-fixedWidthDemo.args = {
-  title: '标题',
-  size: 'fixed',
 };
