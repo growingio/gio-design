0ab53e2732c06b2c77d2ad78e101a17e39e0646c
vermilionAnd <zhujiahong@growingio>
2022-04-13 15:00:09 +0800

    docs(datePicker): add datePicker storybook

--- src/date-picker/demos/DatePicker.stories.tsx
+++ src/date-picker/demos/DatePicker.stories.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { withDesign } from 'storybook-addon-designs';
-import { DownFilled } from '@gio-design/icons';
 import { addMonths, isBefore, startOfToday } from 'date-fns';
+import { action } from '@storybook/addon-actions';
+import { FoldOutlined,DownOutlined } from '@gio-design/icons';
 import Docs from './DatePickerPage';
 import Button from '../../button';
 import Toast from '../../toast';
 import DatePicker, { DatePickerProps } from '../index';
-
 import '../style';
 import '../../button/style';
 import '../../toast/style';
@@ -30,42 +30,89 @@ export default {
 
 const Template: Story<DatePickerProps> = (args) => <DatePicker {...args} />;
 
-export const NormalPicker = Template.bind({});
-NormalPicker.args = {
+export const Default = Template.bind({});
+Default.args = {
   defaultValue: new Date(),
-  placeholder: 'please select',
-  onClear: () => {
-    Toast.info('Clear item!');
+  placeholder: '选择具体某一天',
+  onChange: () => {
+    action('onChange')
   },
 };
 
-export const Placeholder = Template.bind({});
-Placeholder.args = {
-  placeholder: 'please select',
+export const Format: Story<DatePickerProps>  = (args)=>{
+  const [value,setValue]= useState(new Date());
+
+  return <DatePicker
+    value={value}
+    onSelect={setValue}
+    {...args}
+  />
+}
+Format.args={
+  format:'yyyy/dd-mm'
+}
+
+export const PrefixAndSuffix = Template.bind({});
+PrefixAndSuffix.args = {
+  defaultValue: new Date(),
+  prefix: <FoldOutlined />,
+  suffix: <DownOutlined />
+};
+
+export const AllowClear =Template;
+AllowClear.args = {
+  allowClear:true,
+};
+
+export const Size = Template.bind({});
+Size.args = {
+  defaultValue: new Date(),
+  size:'small'
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled:true,
+  disabledDate: (current: Date) => current.getTime() > new Date().getTime(),
+  placeholder: '选择器Disabled',
+  onClear: () => {
+    Toast.info('Clear date!');
+  },
+};
+
+export const OnSelectAndOnClose = Template.bind({});
+OnSelectAndOnClose.args = {
+  allowClear:'true',
+  onSelect:action('onSelect'),
+  onChange:action('onChange'),
+  onClear: action('onClear'),
 };
 
 const CustomizeTriggerTemplate: Story<DatePickerProps> = (args) => {
-  const [value, setValue] = useState('please select');
-
-  const onSelect = (date: Date, dateString: string) => {
-    setValue(dateString);
+  const [value,setValue]= useState(new Date().toString());
+  const onSelect = (e:any) => {
+    setValue(e.toString())
+    action('onSelect:')
   };
-
+  const onChange = (e:any) => {
+    action(e)
+  };
   return (
     <DatePicker
       trigger={
-        <Button type="secondary">
-          {value} <DownFilled size="14px" />
+        <Button type="secondary">您的所选时间为
+          {value}
         </Button>
       }
       onSelect={onSelect}
+      onChange={onChange}
       {...args}
     />
   );
 };
 
-export const CustomizeTrigger = CustomizeTriggerTemplate.bind({});
-CustomizeTrigger.args = {};
+export const CustomTrigger = CustomizeTriggerTemplate.bind({});
+CustomTrigger.args = {};
 
 const StaticTemplate: Story = (args) => <DatePicker.Static {...args} />;
 
