31da81e303cf80817aeb6ba0985e017d4856ece0
Danny <liudan@growingio.com>
2022-04-21 15:26:21 +0800

    doc: searchBar

--- src/search-bar/demos/SearchBarPage.tsx
+++ src/search-bar/demos/SearchBarPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Canvas, Title, Heading, Story, Subheading, ArgsTable } from '@storybook/addon-docs';
+import { Canvas, Title, Heading, Story, Subheading, ArgsTable, Description, Subtitle } from '@storybook/addon-docs';
 import { useIntl } from 'react-intl';
+import { Figma } from 'storybook-addon-designs/esm/blocks';
 import SearchBar from '../index';
 
 export default function ListPage() {
@@ -8,23 +9,13 @@ export default function ListPage() {
 
   return (
     <>
-      <Title>{formatMessage({ defaultMessage: 'SearchBar 分页' })}</Title>
-      <p>
-        {formatMessage({
-          defaultMessage: '在所有内容中，通过输入内容的关键信息筛选出某个内容。',
-        })}
-      </p>
-      <p>
-        <a href="https://www.figma.com/file/lLYusioN7e9ifkQnIXeT4G/GIO-Design-(Running-File)?node-id=4078%3A43861">
-          Figma
-        </a>
-      </p>
-      <p>Upgrading Guide</p>
-      <ul>
-        <li>样式：两种大小，新边框样式</li>
-        <li>Api：删除冗余api</li>
-        <li>包装的input，type=text，</li>
-      </ul>
+      <Title>{formatMessage({ defaultMessage: 'Search Bar 搜索框' })}</Title>
+      <Subtitle>{formatMessage({ defaultMessage: '设计稿' })}</Subtitle>
+      <Figma
+        height="50%"
+        collapsable
+        url="https://www.figma.com/file/kP3A6S2fLUGVVMBgDuUx0f/GIO-Design?node-id=4078%3A43861"
+      />
       <Heading>{formatMessage({ defaultMessage: '代码演示' })}</Heading>
       <Subheading>{formatMessage({ defaultMessage: '样例展示' })}</Subheading>
       <Canvas>
