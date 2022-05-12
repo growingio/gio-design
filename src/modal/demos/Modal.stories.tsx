4c102d1d6372ac40e30594136f8729d6fc0d1538
vermilionAnd <61216850+zhuzilv@users.noreply.github.com>
2022-05-12 17:00:38 +0800

    docs(modal): add storybook (#1977)
    Co-authored-by: vermilionAnd <zhujiahong@growingio>

--- src/modal/demos/Modal.stories.tsx
+++ src/modal/demos/Modal.stories.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import React, { useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { PlusCircleFilled } from '@gio-design/icons';
+import { action } from '@storybook/addon-actions';
 import Modal from '../index';
 import Button from '../../button';
 import { ModalProps, IModalStaticFuncConfig } from '../interface';
@@ -17,7 +19,7 @@ export default {
   },
 } as Meta;
 
-const Template: Story<ModalProps> = (args) => {
+export const Default: Story<ModalProps> = (args) => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
@@ -31,24 +33,64 @@ const Template: Story<ModalProps> = (args) => {
         onOk={() => {
           setVisible(false);
         }}
-      >
-        宽度自动撑开 宽度自动撑开
-      </Modal>
+      />
     </div>
   );
 };
-export const AdaptiveWidthDemo = Template.bind({});
-AdaptiveWidthDemo.args = {
-  title: '弹窗标题',
-};
 
-const FixedTemplate: Story<ModalProps> = (args) => {
+export const CustomButton: Story<ModalProps> = () => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
       <Button onClick={() => setVisible(true)}>Open Modal</Button>
       <Modal
-        {...args}
+        title="customTitle"
+        confirmLoading
+        okText="customText"
+        cancelText="customText"
+        okType="text"
+        closeIcon={<PlusCircleFilled />}
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+        onOk={() => {
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+};
+
+export const OnCloseAndOnOk: Story<ModalProps> = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setVisible(true)}>Open Modal</Button>
+      <Modal
+        afterClose={action('afterClose')}
+        visible={visible}
+        onClose={() => {
+          action('OK');
+          setVisible(false);
+        }}
+        onOk={() => {
+          action('Close');
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+};
+
+export const FixedTemplate: Story<ModalProps> = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setVisible(true)}>Open Modal</Button>
+      <Modal
+        title="弹窗标题"
+        size="fixed"
         visible={visible}
         onClose={() => {
           setVisible(false);
@@ -63,25 +105,74 @@ const FixedTemplate: Story<ModalProps> = (args) => {
   );
 };
 
-export const FixedWidthDemo = FixedTemplate.bind({});
-FixedWidthDemo.args = {
-  title: '弹窗标题',
-  size: 'fixed',
-};
-
-export const FullModal = Template.bind({});
-FullModal.args = {
-  title: '弹窗标题',
-  size: 'full',
-};
-
-const HeightOverflowModalTemplate: Story<ModalProps> = (args) => {
+export const FixedWidthDemo: Story<ModalProps> = () => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
       <Button onClick={() => setVisible(true)}>Open Modal</Button>
       <Modal
-        {...args}
+        title="弹窗标题"
+        size="fixed"
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+        onOk={() => {
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+};
+
+export const WidthAuto: Story<ModalProps> = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setVisible(true)}>Open Modal</Button>
+      <Modal
+        title="弹窗标题"
+        size="normal"
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+        onOk={() => {
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+};
+
+export const FullModal: Story<ModalProps> = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setVisible(true)}>Open Modal</Button>
+      <Modal
+        title="弹窗标题"
+        size="full"
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+        onOk={() => {
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+};
+
+export const HeightOverflowModal: Story<ModalProps> = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setVisible(true)}>Open Modal</Button>
+      <Modal
+        title="弹窗标题"
+        size="fixed"
         visible={visible}
         onClose={() => {
           setVisible(false);
@@ -98,43 +189,10 @@ const HeightOverflowModalTemplate: Story<ModalProps> = (args) => {
   );
 };
 
-export const HeightOverflowModal = HeightOverflowModalTemplate.bind({});
-HeightOverflowModal.args = {
-  title: '弹窗标题',
-  size: 'fixed',
-};
-
-const ConfirmTemplate: Story<ModalProps> = () => (
-  <div>
-    <Button
-      onClick={() => {
-        Modal.open({
-          title: '弹窗标题',
-          content: 'Some descriptions',
-          size: 'fixed',
-          onOk() {
-            console.log('OK');
-          },
-          onClose() {
-            console.log('Cancel');
-          },
-        });
-      }}
-    >
-      Open Modal
-    </Button>
-  </div>
-);
-export const OpenModal = ConfirmTemplate.bind({});
-OpenModal.args = {
-  title: '弹窗标题',
-};
-
-const UseModalTemplate: Story<IModalStaticFuncConfig> = (args) => {
+export const UseModal: Story<IModalStaticFuncConfig> = () => {
   const [modalFuncs, hookModal] = Modal.useModal();
   const handleConfirm = () => {
     modalFuncs.open({
-      ...args,
       title: '弹窗标题',
       content: 'Confirm content',
     });
@@ -143,80 +201,9 @@ const UseModalTemplate: Story<IModalStaticFuncConfig> = (args) => {
   return (
     <>
       <Button type="secondary" onClick={() => handleConfirm()}>
-        open
+        Open Modal Hook
       </Button>
       {hookModal}
-    </>
-  );
-};
-
-export const UseModal = UseModalTemplate.bind({});
-UseModal.args = {};
-
-export const Demo = () => {
-  const [modalFuncs, hookModal] = Modal.useModal();
-  const handleConfirm = () => {
-    modalFuncs.open({
-      title: '弹窗标题',
-      content: 'Confirm content',
-      size: 'fixed',
-    });
-  };
-  return (
-    <>
-      <table className="table-demo">
-        <tr>
-          <th>Controls</th>
-          <th>Example</th>
-        </tr>
-        <tr>
-          <td>AdaptiveWidthDemo</td>
-          <td>
-            <AdaptiveWidthDemo title="AdaptiveWidthDemo" />
-          </td>
-        </tr>
-        <tr>
-          <td>FixedTemplate</td>
-          <td>
-            <FixedTemplate size="fixed" title="FixedTemplate" />
-          </td>
-        </tr>
-        <tr>
-          <td>FullModal</td>
-          <td>
-            <FullModal title="FullModal" />
-          </td>
-        </tr>
-        <tr>
-          <td>HeightOverflowModalTemplate</td>
-          <td>
-            <HeightOverflowModalTemplate size="full" title="HeightOverflowModalTemplate" />
-          </td>
-        </tr>
-        <tr>
-          <td>ConfirmTemplate</td>
-          <td>
-            <ConfirmTemplate size="fixed" />
-          </td>
-        </tr>
-        <tr>
-          <td>OpenModal(</td>
-          <td>
-            <OpenModal />
-          </td>
-        </tr>
-        <tr>
-          <td>UseModalTemplate</td>
-          <td>
-            <>
-              <Button type="secondary" onClick={() => handleConfirm()}>
-                open
-              </Button>
-              {hookModal}
-            </>
-          </td>
-        </tr>
-      </table>
     </>
   );
 };
