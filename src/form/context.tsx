1a88526ae85b731efa5a12099c8ad2e3f9489656
gavin <haozhigang@outlook.com>
2022-05-23 11:53:19 +0800

    test(form): add test cases (#2019)
    * test(checkbox): add test case
    
    * test(result): add test cases
    
    * refactor(result): remove test file unused references
    
    * test(switch): add test cases
    
    * test(form): add test cases
    
    * ci(sonar): ignore case
    
    * ci(sonar): config ignore rule

--- src/form/context.tsx
+++ src/form/context.tsx
@@ -1,6 +1,3 @@
-import { FormProvider as RcFormProvider } from 'rc-field-form';
-import { omit } from 'lodash';
-import { FormProviderProps as RcFormProviderProps } from 'rc-field-form/lib/FormContext';
 import React from 'react';
 
 import { FormContextProps } from './interface';
@@ -13,11 +10,3 @@ export const FormContext = React.createContext<FormContextProps>({
   labelAlign: 'left',
   layout: 'vertical',
 });
-
-export type FormProviderProps = Omit<RcFormProviderProps, 'validateMessages'>;
-
-export const FormProvider: React.FC<FormProviderProps> = (props) => {
-  const providerProps = omit(props, 'prefixCls');
-
-  return <RcFormProvider {...providerProps} />;
-};
