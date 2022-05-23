77c6e3453a2ebca6f95f853660aa0256f4a6abf4
vermilionAnd <61216850+zhuzilv@users.noreply.github.com>
2022-05-23 15:43:26 +0800

    docs(steps): add steps storybook (#2013)
    Co-authored-by: vermilionAnd <zhujiahong@growingio>

--- src/steps/demos/StepsPage.tsx
+++ src/steps/demos/StepsPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Canvas, Title, Heading, Story, Subheading, ArgsTable } from '@storybook/addon-docs';
+import { Canvas, Title, Heading, Story, Subheading, ArgsTable, Description, Subtitle } from '@storybook/addon-docs';
+import { Figma } from 'storybook-addon-designs/esm/blocks';
 import { useIntl } from 'react-intl';
 import Steps from '../Steps';
 
@@ -9,43 +10,68 @@ export default function ListPage() {
   return (
     <>
       <Title>{formatMessage({ defaultMessage: 'Steps 步骤条' })}</Title>
-      <p>
+      <Description>
         {formatMessage({
           defaultMessage: '分步完成复杂的表单操作流程 用于切换下方的内容',
         })}
-      </p>
-      <p>
-        <a href="https://www.figma.com/file/lLYusioN7e9ifkQnIXeT4G/GIO-Design-(Running-File)?node-id=6341%3A62729">
-          Figma
-        </a>
-      </p>
-      <p>Upgrading Guide</p>
-      <ul>
-        <li>包装自tabs，样式调整，</li>
-        <li>Steps 组件基于 Tabs 组件（size = normal)，增加是否完成的判断，完成的步骤有 icon、未完成步骤没有。</li>
-        <li>Steps 组件在使用时，通常只有当前步骤完成后，下一步骤才会解除 disabled 状态。</li>
-        <li>current 指定当前步骤，从 1 开始记数。</li>
-      </ul>
+      </Description>
+      <Subtitle>{formatMessage({ defaultMessage: '设计稿' })}</Subtitle>
+      <Figma
+        height="50%"
+        collapsable
+        url="https://www.figma.com/file/lLYusioN7e9ifkQnIXeT4G/GIO-Design-(Running-File)?node-id=6341%3A62729"
+      />
+
       <Heading>{formatMessage({ defaultMessage: '代码演示' })}</Heading>
-      <Subheading>{formatMessage({ defaultMessage: '样例展示' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-steps--demo" />
-      </Canvas>
+      <Subheading>{formatMessage({ defaultMessage: '基本样式' })}</Subheading>
       <Subheading>{formatMessage({ defaultMessage: 'default' })}</Subheading>
       <Canvas>
         <Story id="upgraded-steps--default" />
       </Canvas>
-      {/* <Subheading>{formatMessage({ defaultMessage: 'no-tab' })}</Subheading>
-       <Canvas>
-        <Story id="upgraded-steps--no-tab" />
-      </Canvas>
-      <Subheading>{formatMessage({ defaultMessage: 'no-prefix' })}</Subheading>
-      <Canvas>
-        <Story id="upgraded-steps--no-prefix" />
-      </Canvas>  */}
-      <Subheading>{formatMessage({ defaultMessage: 'have-children' })}</Subheading>
+      <Subheading>{formatMessage({ defaultMessage: '有内容时' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: '可以在Steps.step中添加子元素作为内容切换',
+        })}
+      </Description>
       <Canvas>
         <Story id="upgraded-steps--have-children" />
+      </Canvas>
+      <Subheading>{formatMessage({ defaultMessage: '有点击按钮交互写法' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: '可以结合实际情况添加button完成步骤交互',
+        })}
+      </Description>
+      <Canvas>
+        <Story id="upgraded-steps--have-step-button" />
+      </Canvas>
+      <Subheading>{formatMessage({ defaultMessage: '点击事件' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: '`onChange`为点击事件，可查看console控制台查看',
+        })}
+      </Description>
+      <Canvas>
+        <Story id="upgraded-steps--on-change" />
+      </Canvas>
+      <Subheading>{formatMessage({ defaultMessage: '受控模式value' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: '`value`为受控模式的值，`current`为当前步骤，可以在current不改变情况下，切换当前key',
+        })}
+      </Description>
+      <Canvas>
+        <Story id="upgraded-steps--value" />
+      </Canvas>
+      <Subheading>{formatMessage({ defaultMessage: '大小' })}</Subheading>
+      <Description>
+        {formatMessage({
+          defaultMessage: '`size`有两种，normal高度为36px，small高度为30px',
+        })}
+      </Description>
+      <Canvas>
+        <Story id="upgraded-steps--size" />
       </Canvas>
       <Heading>{formatMessage({ defaultMessage: '参数说明' })}</Heading>
       <ArgsTable of={Steps} />
