cc9c5aa2037ed5036c60ff50be1608a3479ec902
haozhigang <haozhigang@outlook.com>
2022-06-07 19:52:47 +0800

    refactor(collapse): fix eslint error

--- src/collapse/__tests__/Collapse.test.tsx
+++ src/collapse/__tests__/Collapse.test.tsx
@@ -1,8 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import Collapse from '..';
-import { sleep } from '../../utils/test';
 
 describe('Collapse', () => {
   it('renders correctly', () => {
