527236a53b29e8256c622ca2c86cd280cedffdb9
Zhang Rui <zhangrui@growingio.com>
2022-07-28 14:02:06 +0800

    test(Password): update Password UT

--- src/input/__tests__/Password.test.tsx
+++ src/input/__tests__/Password.test.tsx
@@ -7,23 +7,22 @@ describe('testing password', () => {
     const { container } = render(<Password defaultValue="111" />);
     expect(screen.getByTestId('input-password')).toBeTruthy();
     expect(container.querySelector('input[type="password"]')).toBeTruthy();
-    expect(container.querySelector('input[type="password"]').getAttribute('value')).toBe('111')
+    expect(container.querySelector('input[type="password"]').getAttribute('value')).toBe('111');
   });
   it('should change type when click password Suffix icon ', () => {
     const { container } = render(<Password defaultValue="111" />);
-    expect(container.querySelector('span[aria-label="eye-outlined"]')).toBeTruthy();
-    expect(container.querySelector('span[aria-label="eye-slash-outlined"]')).toBeFalsy();
-    expect(container.querySelector('input[type="password"]')).toBeTruthy();
-    fireEvent.click(container.querySelector('span[aria-label="eye-outlined"]'));
-    expect(container.querySelector('input[type="password"]')).toBeFalsy();
-    expect(container.querySelector('span[aria-label="eye-outlined"]')).toBeFalsy();
     expect(container.querySelector('span[aria-label="eye-slash-outlined"]')).toBeTruthy();
+    expect(container.querySelector('span[aria-label="eye-outlined"]')).toBeFalsy();
+    expect(container.querySelector('input[type="password"]')).toBeTruthy();
+    fireEvent.click(container.querySelector('span[aria-label="eye-slash-outlined"]'));
+    expect(container.querySelector('input[type="password"]')).toBeFalsy();
+    expect(container.querySelector('span[aria-label="eye-slash-outlined"]')).toBeFalsy();
+    expect(container.querySelector('span[aria-label="eye-outlined"]')).toBeTruthy();
   });
   it('should not change type when  password set disabled ', () => {
     const { container } = render(<Password defaultValue="111" disabled />);
 
-    fireEvent.click(container.querySelector('span[aria-label="eye-outlined"]'));
+    fireEvent.click(container.querySelector('span[aria-label="eye-slash-outlined"]'));
     expect(container.querySelector('input[type="password"]')).toBeTruthy();
-
-  })
-})
+  });
+});
