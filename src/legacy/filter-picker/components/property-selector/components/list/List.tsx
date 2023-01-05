91321f1dd8aa95a64e353eb79822f2874228fc33
Jack <i@jackpan.me>
2023-01-05 13:48:51 +0000

    style: fix bug from sonar

--- src/legacy/filter-picker/components/property-selector/components/list/List.tsx
+++ src/legacy/filter-picker/components/property-selector/components/list/List.tsx
@@ -10,25 +10,24 @@ import { ListProps, ListItemProps, ListItemGroupProps } from './interfaces';
 import { useRootPrefixCls } from './utils';
 
 function InternalList({ className, style, children, items, expandable = false }: ListProps) {
-  function renderItems(currentItems: (ListItemGroupProps | ListItemProps)[]) {
-    return currentItems.map((item: ListItemGroupProps | ListItemProps, index) => {
-      if ('title' in item) {
-        const itemGroup = item as ListItemGroupProps;
-        return <ItemGroup expandable={expandable} {...itemGroup} />;
-      }
-      return <Item key={`item-${index}`} {...(item as ListItemProps)} />;
-    });
-  }
 
-  let content;
-  if (items && items.length > 0) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks, react-hooks/exhaustive-deps
-    content = React.useMemo(() => renderItems(items), [items, expandable]);
-  } else if (children) {
-    content = children;
-  } else {
-    content = <Result size="small" type="empty" />;
-  }
+  const content = React.useMemo(() => {
+    if (children) {
+      return children;
+    }
+
+    if (items && items.length > 0) {
+      return items.map((item: ListItemGroupProps | ListItemProps, index) => {
+        if ('title' in item) {
+          const itemGroup = item as ListItemGroupProps;
+          return <ItemGroup expandable={expandable} {...itemGroup} />;
+        }
+        return <Item key={`item-${index}`} {...(item as ListItemProps)} />;
+      });
+    }
+
+    return <Result size="small" type="empty" />;
+  }, [items, expandable,children]);
 
   const cls = classnames(useRootPrefixCls(), className);
   return (
