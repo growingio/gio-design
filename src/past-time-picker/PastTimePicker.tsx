9e7fc5c3df1d6c4265075d6da9cfa1aaf1ed559e
YanHui <liubai@startdt.com>
2024-07-04 10:29:56 +0800

    fix(pasttimepicker): fix onclear

--- src/past-time-picker/PastTimePicker.tsx
+++ src/past-time-picker/PastTimePicker.tsx
@@ -83,7 +83,7 @@ const PastTimePicker = (props: PastTimePickerProps) => {
 
   const [controlledVisible, setControlledVisible] = useControlledState(popoverVisible, false);
 
-  const [timeRange, setTimeRange] = useControlledState<string | undefined | null>(value, undefined);
+  const [timeRange, setTimeRange] = useControlledState<string | undefined>(value, undefined);
 
   const QUICK_MAPPING = {
     'day:1,0': todayText,
@@ -171,7 +171,7 @@ const PastTimePicker = (props: PastTimePickerProps) => {
   };
 
   const handleOnSelect = (currentValue: string | null) => {
-    setTimeRange(currentValue, true);
+    setTimeRange(currentValue ?? undefined, true);
     setControlledVisible(false);
     onSelect?.(currentValue);
   };
